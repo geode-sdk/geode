6a388e1c3e7b6d959681d839ccaac4a48c9a5081
Alkım Deniz Altıntaş <altalk.23@robcol.k12.tr>
2022-01-20 10:40:57 +0300

    move these two

---
+++ SDK/Include/helpers/convert.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <Geode>
+
+namespace geode::cocos {
+    void ccDrawColor4B(cocos2d::ccColor4B const& color);
+    cocos2d::ccColor4B invert4B(cocos2d::ccColor4B const& color);
+    cocos2d::ccColor3B invert3B(cocos2d::ccColor3B const& color);
+    cocos2d::ccColor3B to3B(cocos2d::ccColor4B const& color);
+    cocos2d::ccColor4B to4B(cocos2d::ccColor3B const& color, GLubyte alpha = 255);
+    cocos2d::ccColor4F to4F(cocos2d::ccColor4B const& color);
+    constexpr cocos2d::ccColor3B cc3x(int hexValue) {
+        if (hexValue <= 0xf)
+            return cocos2d::ccColor3B {
+                static_cast<GLubyte>(hexValue * 17),
+                static_cast<GLubyte>(hexValue * 17),
+                static_cast<GLubyte>(hexValue * 17)
+            };
+        if (hexValue <= 0xff)
+            return cocos2d::ccColor3B {
+                static_cast<GLubyte>(hexValue),
+                static_cast<GLubyte>(hexValue),
+                static_cast<GLubyte>(hexValue)
+            };
+        if (hexValue <= 0xfff)
+            return cocos2d::ccColor3B {
+                static_cast<GLubyte>((hexValue >> 8 & 0xf) * 17),
+                static_cast<GLubyte>((hexValue >> 4 & 0xf) * 17),
+                static_cast<GLubyte>((hexValue >> 0 & 0xf) * 17)
+            };
+        else
+            return cocos2d::ccColor3B {
+                static_cast<GLubyte>(hexValue >> 16 & 0xff),
+                static_cast<GLubyte>(hexValue >> 8  & 0xff),
+                static_cast<GLubyte>(hexValue >> 0  & 0xff)
+            };
+    }
+
+    template<typename T>
+    static cocos2d::CCArray* vectorToCCArray(std::vector<T> const& vec) {
+        auto res = cocos2d::CCArray::createWithCapacity(vec.size());
+        for (auto const& item : vec)
+            res->addObject(item);
+        return res;
+    }
+
+    template<typename T, class T2>
+    static cocos2d::CCArray* vectorToCCArray(std::vector<T> const& vec, std::function<T2(T)> convFunc) {
+        auto res = cocos2d::CCArray::createWithCapacity(vec.size());
+        for (auto const& item : vec)
+            res->addObject(convFunc(item));
+        return res;
+    }
+
+    template <typename T>
+	std::vector<T> ccArrayToVector(CCArray* arr) {
+		return std::vector<T>(reinterpret_cast<T*>(arr->data->arr), reinterpret_cast<T*>(arr->data->arr) + arr->data->num);
+	}
+}
