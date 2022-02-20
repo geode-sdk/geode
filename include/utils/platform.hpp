dc6651b4e83b0711b2eeb00132172c803a0f080a
camila314 <camden7306@gmail.com>
2022-02-19 21:10:39 -0600

    move geode dir to documents on mac

--- include/utils/platform.hpp
+++ include/utils/platform.hpp
@@ -5,8 +5,13 @@
 #include <string>
 #include <vector>
 #include <functional>
+#include <fs/filesystem.hpp>
 
 namespace geode::utils::clipboard {
     GEODE_DLL bool write(std::string const& data);
     GEODE_DLL std::string read();
+}
+
+namespace geode::utils::dirs {
+    GEODE_DLL ghc::filesystem::path geode_root();
 }
