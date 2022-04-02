2109e5dfc8f448dc8962f0bfd6387825340dd6fd
altalk23 <altalk23@users.noreply.github.com>
2022-04-03 01:31:39 +0300

    nvm i didnt

--- kit/gen/AddressGen.cpp
+++ kit/gen/AddressGen.cpp
@@ -28,6 +28,6 @@ int main(int argc, char** argv) {
 			);
 		}
 	}
-    cout << output << enld;
+    cout << output << endl;
 	CacShare::writeFile(output);
 }
--- kit/gen/TypeGen.cpp
+++ kit/gen/TypeGen.cpp
@@ -65,6 +65,5 @@ int main(int argc, char** argv) {
 			);
 		}
 	}
-    cout << output << endl;
 	CacShare::writeFile(output);
 }
