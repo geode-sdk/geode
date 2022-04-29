e28052c2fa752197b63c65008fcf9cb653c835bc
HJfod <60038575+HJfod@users.noreply.github.com>
2022-04-29 15:04:39 +0300

    remove stream.hpp and add color logging to log.hpp

--- include/utils/stream.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Geode.hpp>
-
-namespace geode::stream {
-    GEODE_DLL std::ostream& operator<<(std::ostream& stream, cocos2d::CCPoint const& pos);
-    GEODE_DLL std::ostream& operator<<(std::ostream& stream, cocos2d::CCSize  const& size);
-    GEODE_DLL std::ostream& operator<<(std::ostream& stream, cocos2d::CCRect  const& rect);
-}
