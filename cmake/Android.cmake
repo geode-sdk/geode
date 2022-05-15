624ffba6db120d17384fd145bb90bce57135fb00
matcool <26722564+matcool@users.noreply.github.com>
2022-05-15 17:00:28 -0300

    remove old stuff from Android.cmake

--- cmake/Android.cmake
+++ cmake/Android.cmake
@@ -11,45 +11,8 @@ include_directories(
 
 macro(link_prebuilts project)
 	target_link_libraries(${project} 
-		${CMAKE_SOURCE_DIR}/lib/android/libcocos2dcpp.so
+		${GEODE_SDK_PATH}/link/libcocos2dcpp.so
 	)
 endmacro()
 
 link_prebuilts(${PROJECT_NAME})
-
-include_directories(
-	"${GEODE_ANDROID_NDK}/toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include"
-	"${GEODE_ANDROID_NDK}/toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include/arm-linux-androideabi"
-	"${GEODE_ANDROID_NDK}/toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include/c++/v1"
-)
-
-set(PACKAGE_DIR ${CMAKE_CURRENT_BINARY_DIR}/AndroidBuild)
-set(OUT_FILE "${PROJECT_NAME}_${CMAKE_PROJECT_VERSION}-3.apk")
-
-add_custom_command(
-	COMMAND python3 ${CMAKE_SOURCE_DIR}/kit/pkg/Android.py "${PACKAGE_DIR}" "${CMAKE_CURRENT_BINARY_DIR}"  "${PROJECT_NAME}" "${GEODE_APK_FILE}" "${OUT_FILE}"
-	DEPENDS ${GEODE_ANDROID_CONTROL}
-	OUTPUT  ${PACKAGE_DIR}/_
-	COMMENT "Packaging ${PROJECT_NAME}"
-)
-
-add_custom_target(
-	AndroidPackage ALL
-	DEPENDS ${PACKAGE_DIR}/_
-)
-
-add_dependencies(AndroidPackage ${PROJECT_NAME})
-
-add_custom_command(
-	COMMAND adb install "${PACKAGE_DIR}/${PROJECT_NAME}_${CMAKE_PROJECT_VERSION}-3-aligned-debugSigned.apk"
-	WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-	COMMENT "Install target ${PROJECT_NAME}"
-	OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/run0
-)
-
-add_custom_target(
-	Install ALL
-	DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/run0
-)
-
-add_dependencies(Install AndroidPackage)
