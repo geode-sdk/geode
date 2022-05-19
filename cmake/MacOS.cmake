82f627c0adff6f32c49cc4ef53d8946d71c0087f
camila314 <camden7306@gmail.com>
2022-05-19 03:39:55 -0500

    does life really have to be this hard

--- cmake/MacOS.cmake
+++ cmake/MacOS.cmake
@@ -1,7 +1,7 @@
 add_definitions(-DCC_TARGET_OS_MAC)
 set(CMAKE_SYSTEM_NAME MacOS)
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fms-extensions -Wno-deprecated -Wno-ignored-attributes -O2 -Os -fdata-sections -ffunction-sections")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fms-extensions -Wno-deprecated -Wno-ignored-attributes -arch x86_64 -O2 -Os -fdata-sections -ffunction-sections")
 set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -dead_strip")
 
 include_directories(
