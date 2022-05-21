6240a8de08a600d99fd8bdcc588c58080b2bf740
xhayper <hayper1919@gmail.com>
2022-05-21 12:20:36 +0700

    fix(cmake): Not working on mac platform

--- cmake/MacOS.cmake
+++ cmake/MacOS.cmake
@@ -1,8 +1,9 @@
 add_definitions(-DCC_TARGET_OS_MAC)
 set(CMAKE_SYSTEM_NAME MacOS)
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fms-extensions -Wno-deprecated -Wno-ignored-attributes -arch x86_64 -O2 -Os -fdata-sections -ffunction-sections")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fms-extensions -Wno-deprecated -Wno-ignored-attributes -O2 -Os -fdata-sections -ffunction-sections")
 set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -dead_strip")
+set(CMAKE_OSX_ARCHITECTURES x86_64)
 
 include_directories(
 	${GEODE_SDK_PATH}/include/cocos/cocos2dx/platform/mac
