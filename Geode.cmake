669d3525d6a7f0318e7ecd46eb1d4ad252f23507
xhayper <hayper1919@gmail.com>
2022-05-24 21:31:20 +0700

    feat(CODEGEN): Add an option to disable adding fmt as subdirectory, because some project use fmt

--- Geode.cmake
+++ Geode.cmake
@@ -5,10 +5,11 @@
 		GEODE_CODEGEN_DIR         location of codegen directory
 	
 	geode-imported variables
-		OUTPUT                    name of the output binary
-		BUILD_CODEGEN             whether to build 
-		NO_GEODE_FILE             dont produce geode file
-		NO_LOADER                 dont link loader
+		OUTPUT                    													name of the output binary
+		BUILD_CODEGEN             													whether to build
+		NO_GEODE_FILE             													dont produce geode file
+		NO_LOADER                 													dont link loader
+		DISABLE_THE_LINKING_OF_THE_EXTERNAL_DEPENDENCY_KNOWN_AS_FMTLIB				dont add fmt subdirectory
 		
 ]]
 
@@ -20,7 +21,7 @@ include(CMakeParseArguments)
 include(${GEODE_SDK_PATH}/cmake/GeodeFile.cmake)
 
 function(setup_geode_mod)
-	set(bools BUILD_CODEGEN NO_GEODE_FILE NO_LOADER NO_PRECOMPILED_HEADER NO_ENTRY)
+	set(bools BUILD_CODEGEN NO_GEODE_FILE NO_LOADER NO_PRECOMPILED_HEADER NO_ENTRY DISABLE_THE_LINKING_OF_THE_EXTERNAL_DEPENDENCY_KNOWN_AS_FMTLIB)
     cmake_parse_arguments(
         PARSED_ARGS
         "${bools}"
@@ -33,6 +34,7 @@ function(setup_geode_mod)
     set(GEODE_DO_CODEGEN ${PARSED_ARGS_BUILD_CODEGEN})
     set(GEODE_NO_PRECOMPILED_HEADER ${PARSED_ARGS_NO_PRECOMPILED_HEADER})
     set(GEODE_NO_ENTRY ${PARSED_ARGS_NO_ENTRY})
+	set(GEODE_DISABLE_THE_LINKING_OF_THE_EXTERNAL_DEPENDENCY_KNOWN_AS_FMTLIB ${PARSED_ARGS_DISABLE_THE_LINKING_OF_THE_EXTERNAL_DEPENDENCY_KNOWN_AS_FMTLIB})
 	if(PARSED_ARGS_OUTPUT)
 		set_target_properties(${PROJECT_NAME} PROPERTIES PREFIX "" OUTPUT_NAME ${PARSED_ARGS_OUTPUT})
 	endif()
