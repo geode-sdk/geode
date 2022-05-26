ff5bbe61fd3cddec8d0d804922a690f3430646df
camila314 <camden7306@gmail.com>
2022-05-26 16:23:05 -0500

    use current instead of root

--- Geode.cmake
+++ Geode.cmake
@@ -46,7 +46,6 @@ function(setup_geode_mod)
 	endif()
 
 	if (${GEODE_DO_CODEGEN})
-
 		# only 1 codegen dir
 		get_property(GEODE_CODEGEN_DIR GLOBAL PROPERTY GEODE_CODEGEN_DIR)
 		if (NOT GEODE_CODEGEN_DIR)
@@ -60,7 +59,7 @@ function(setup_geode_mod)
 
 		set_source_files_properties(${GEODE_CODEGEN_DIR}/GeneratedSource.cpp PROPERTIES GENERATED 1)
 		target_sources(${PROJECT_NAME} PRIVATE ${GEODE_CODEGEN_DIR}/GeneratedSource.cpp)
-		target_include_directories(${PROJECT_NAME} PUBLIC ${CMAKE_BINARY_DIR})
+		target_include_directories(${PROJECT_NAME} PUBLIC ${CMAKE_CURRENT_BINARY_DIR})
 		#target_link_libraries(${PROJECT_NAME} fmt)
 
 		if (DEFINED GEODE_CODEGEN_BUILD)
