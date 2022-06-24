eb1df56b5bae2bd78cfed58da1eb5c76c01ca189
altalk23 <altalk23@users.noreply.github.com>
2022-06-25 00:54:36 +0300

    another field reimplementation yay

--- include/syntax/Field.hpp
+++ include/syntax/Field.hpp
@@ -1,139 +1,60 @@
 #pragma once
+#include "../Geode.hpp"
 #include "Traits.hpp"
-#include "../platform/platform.hpp"
-
-#ifdef GEODE_IS_WINDOWS
-// for the manual virtualprotect
-#include <memoryapi.h>
-#endif
+#include <vector>
 
 namespace geode::modifier {
-	template<typename T = void*>
-	struct container; 
-	template<typename T>
-	struct field;
+	class FieldContainer : public cocos2d::CCObject {
+		std::vector<void*> m_containedFields;
+	public:
 
-	template<typename T>
-	struct container {
-	    T* field;
-		container() {
-			field = new T();
-		}
-		container(T value) {
-			field = new T();
-			*field = value;
-		}
-	    virtual ~container() {
-	        delete field;
-	    }
-	    T& get() {
-	    	return *field;
-	    }
-	};
-
-	using FieldDefault = std::pair<void*, container<void*>*>;
-
-	GEODE_NOINLINE inline auto& fieldDefaultQueue() {
-    	static std::vector<FieldDefault> ret;
-    	return ret;
-    }
-
-	template<typename T>
-	struct field {
-		uint8_t padding;
-
-		field() {
-			auto cont = new container<T>();
-			fieldDefaultQueue().push_back(
-				{this, (container<void*>*)(cont)}
-			);
-		}
-
-		field(T defaultValue) {
-			auto cont = new container<T>(defaultValue);
-			fieldDefaultQueue().push_back(
-				{this, (container<void*>*)(cont)}
-			);
-		}
-
-	    template<typename Q>
-	    void operator=(Q) {
-	        static_assert(always_false<T>, "field shouldn't be used directly. it should be used with this->*myMember.");
-	    }
-
-	    field<T>* operator->() {
-	        static_assert(always_false<T>, "field shouldn't be used directly. it should be used with this->*myMember.");
+		static FieldContainer* create() {
+			auto ret = new (std::nothrow) FieldContainer;
+			if (ret) {
+				ret->autorelease();
+				return ret;
+			}
 			return nullptr;
-	    }
+		}
+		~FieldContainer() {
+			for(auto ptr : m_containedFields) {
+				operator delete(ptr);
+			}
+		}
+		void* getField(size_t index) {
+			if (m_containedFields.size() <= index) {
+				m_containedFields.resize(index + 1);
+			}
+			return m_containedFields.at(index);
+		}
+		void* setField(size_t index, size_t size) {
+			m_containedFields.at(index) = operator new(size);
+			return m_containedFields.at(index);
+		}
+	};
+	
+	template<class Base, class Intermediate, class Parent>
+	class FieldIntermediate {
+		// Padding used for guaranteeing any member of parents 
+		// will be in between sizeof(Intermediate) and sizeof(Parent)
+		size_t m_padding; 
+	public:
+		template <class=std::enable_if_t<true>>
+		Parent* operator->() {
+			// get the this pointer of the base
+			auto node = reinterpret_cast<cocos2d::CCNode*>(reinterpret_cast<std::byte*>(this) - sizeof(Base));
+			auto container = reinterpret_cast<FieldContainer*>(node->getUserObject());
+			if (!container) {
+				container = FieldContainer::create();
+				node->setUserObject(container);
+			}
+			static size_t index = Loader::get()->getFieldIndexForClass(typeid(Base).hash_code());
+			// this pointer is offset 
+			auto offsetField = container->getField(index);
+			if (!offsetField) offsetField = container->setField(index, sizeof(Parent) - sizeof(Intermediate));
 
-	    void operator*() {
-	        static_assert(always_false<T>, "field shouldn't be used directly. it should be used with this->*myMember.");
-	    } 
-
-	    operator void*() {
-	    	return reinterpret_cast<void*>(this);
-	    }
-
-	    using type = T;
+			return reinterpret_cast<Parent*>(reinterpret_cast<std::byte*>(offsetField) - sizeof(Intermediate));
+		}
 	};
 
-	template <typename T>
-	GEODE_NOINLINE inline auto& getAdditionalContainers() {
-    	static std::unordered_map<T*, std::unordered_map<void*, container<void*>*>> ret;
-    	return ret;
-    }
-
-	using DestructorType = void(GEODE_WINDOWS(__thiscall)*)(void* GEODE_WINDOWS(, bool));
-
-    template <typename T>
-    GEODE_NOINLINE inline auto& getOriginalDestructor() {
-    	static DestructorType ret;
-    	return ret;
-    }
-
-	// this is beautiful
-	struct FieldCleanupHook {
-		template <typename T>
-		static GEODE_NOINLINE inline void GEODE_WINDOWS(__thiscall) fieldCleanup(T* self GEODE_WINDOWS(, bool arg)) {{
-			for (auto& [mem, cont] : getAdditionalContainers<T>()[self]) {
-				delete cont;
-			}
-			getAdditionalContainers<T>().erase(self);
-			getOriginalDestructor<T>()(self GEODE_WINDOWS(, arg));
-		}}
-	};
-
-	template <typename T, typename A>
-	GEODE_NOINLINE T& operator->*(A* self, field<T>& member) {
-	    // this replaces the destructor in the vtable
-	    // only done this way to be performant
-	    // because we are still not using inline hooking
-	    auto& containers = getAdditionalContainers<A>();
-	    auto& destructor = getOriginalDestructor<A>();
-
-	    // on first use
-	    if (destructor == nullptr) {
-			// classes inheriting CCObject always have their destructor(s) as the 2nd (and 3rd on itanium) element in the vtable
-			// choose 3rd elemnt (the second destructor) on itanium platforms because the first one gets inlined often
-			static constexpr size_t index = GEODE_WINDOWS(1) GEODE_MACOS(2) GEODE_IOS(2) GEODE_ANDROID(2);
-	        auto& vtableDestructor = index[*reinterpret_cast<uintptr_t**>(self)]; // i love this
-	        destructor = reinterpret_cast<DestructorType>(vtableDestructor);
-#ifdef GEODE_IS_WINDOWS
-			DWORD old;
-			VirtualProtect(reinterpret_cast<void*>(&vtableDestructor), sizeof(void*), PAGE_EXECUTE_READWRITE, &old);
-#endif
-	        vtableDestructor = reinterpret_cast<uintptr_t>(&FieldCleanupHook::fieldCleanup<A>);
-	    }
-	    if (containers[self].size() == 0) {
-	    	void* obj = new A();
-	    	operator delete(obj);
-			for (auto& [fie, cont] : fieldDefaultQueue()) {
-				containers[self].insert({member, cont});
-			}
-			fieldDefaultQueue().clear();
-	    }
-
-	    // gets the respective field
-	    return reinterpret_cast<container<T>*>(containers[self][member])->get();
-	}
 }
