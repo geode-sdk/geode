91cecf3843d246939be4057cdf8e7d5d607aeeb1
HJfod <60038575+HJfod@users.noreply.github.com>
2022-10-06 22:37:54 +0300

    add CCApplication::get to every platform

--- loader/include/Geode/cocos/cocos2dx/platform/android/CCApplication.h
+++ loader/include/Geode/cocos/cocos2dx/platform/android/CCApplication.h
@@ -32,6 +32,10 @@ public:
     */
     static CCApplication* sharedApplication();
 
+    GEODE_ADD(
+        static CCApplication* get();
+    );
+
     /**
     @brief Get current language config
     @return Current language config
