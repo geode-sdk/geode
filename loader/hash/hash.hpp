3f3314ced826c057d0c425d15aca78cca779e93e
HJfod <60038575+HJfod@users.noreply.github.com>
2022-10-14 21:04:59 +0300

    automatic resource repair  - loader checks if resources are correct at startup, if not, downloads them and replaces  - add sha256 to hash  - change hash to use paths instead of strings  - cmake rework; GeodeFile.cmake now checks CLI version  - add optional `DONT_INSTALL` argument to `create_geode_file`  - test mods are now not installed by default  - add package_geode_resources_now command for packaging resources at configure time and creating a header with their calculated hashes

--- loader/hash/hash.hpp
+++ loader/hash/hash.hpp
@@ -4,11 +4,24 @@
 #include <fstream>
 #include <ciso646>
 #include "picosha3.h"
+#include "picosha2.h"
 #include <vector>
+#include <fs/filesystem.hpp>
 
-static std::string calculateHash(std::string const& path) {
+static std::string calculateSHA3_256(ghc::filesystem::path const& path) {
     std::vector<uint8_t> s(picosha3::bits_to_bytes(256));
     auto sha3_256 = picosha3::get_sha3_generator<256>();
     std::ifstream file(path, std::ios::binary);
     return sha3_256.get_hex_string(file);
+}
+
+static std::string calculateSHA256(ghc::filesystem::path const& path) {
+    std::vector<uint8_t> hash(picosha2::k_digest_size);
+    std::ifstream file(path, std::ios::binary);
+    picosha2::hash256(file, hash.begin(), hash.end());
+    return picosha2::bytes_to_hex_string(hash.begin(), hash.end());
+}
+
+static std::string calculateHash(ghc::filesystem::path const& path) {
+    return calculateSHA3_256(path);
 }
