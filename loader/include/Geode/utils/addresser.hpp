298593ada8ce2846c3792d88d05c4e3fb67ee02d
alk <45172705+altalk23@users.noreply.github.com>
2022-11-30 14:13:20 +0300

    remove the bandaid

--- loader/include/Geode/utils/addresser.hpp
+++ loader/include/Geode/utils/addresser.hpp
@@ -105,12 +105,6 @@ namespace geode::addresser {
                 address = *reinterpret_cast<uint32_t*>(address + 2);
                 // that then contains the actual address of the func
                 address = *reinterpret_cast<uintptr_t*>(address);
-                
-                // used for gd string funcs, pretty temporary
-                if (*reinterpret_cast<uint8_t*>(address) == 0xe9) {
-                    // read the offset and add it
-                    address += 5 + *reinterpret_cast<uint32_t*>(address + 1);
-                }
             }
 #endif
 
