28b413d63635ba609855898c128b710f75c603f6
HJfod <60038575+HJfod@users.noreply.github.com>
2022-12-08 22:04:02 +0200

    make VersionInfo constexpr

--- loader/include/Geode/utils/VersionInfo.hpp
+++ loader/include/Geode/utils/VersionInfo.hpp
@@ -6,66 +6,46 @@
 
 namespace geode {
     /**
-     * Class representing version
-     * information
+     * Class representing version information
      * @class VersionInfo
      */
     class GEODE_DLL VersionInfo {
-    public:
-        enum Compare : char {
-            // Other version is lower or exactly the same
-            Lower,
-            // Other version is exactly the same
-            Exact,
-            // Other version is higher or exactly the same
-            Higher,
-            // Other version does not even have to match lol
-            Any,
-        };
-
     protected:
         int m_major = 1;
         int m_minor = 0;
         int m_patch = 0;
 
     public:
-        VersionInfo() = default;
-        
+        constexpr VersionInfo() = default;
         constexpr VersionInfo(int major, int minor, int patch) {
             m_major = major;
             m_minor = minor;
             m_patch = patch;
         }
-
-        VersionInfo(char const* versionString);
         VersionInfo(std::string const& versionString);
 
-        int getMajor() const;
-        int getMinor() const;
-        int getPatch() const;
+        constexpr int getMajor() const {
+            return m_major;
+        }
 
-        bool operator<(VersionInfo const& other) const;
-        bool operator<=(VersionInfo const& other) const;
-        bool operator>(VersionInfo const& other) const;
-        bool operator>=(VersionInfo const& other) const;
-        bool operator==(VersionInfo const& other) const;
+        constexpr int getMinor() const {
+            return m_minor;
+        }
 
-        bool match(VersionInfo const& other) const;
-        bool match(VersionInfo const& other, VersionInfo::Compare compare) const;
-        bool match(
-            VersionInfo const& other, VersionInfo::Compare major, VersionInfo::Compare minor,
-            VersionInfo::Compare patch
-        ) const;
+        constexpr int getPatch() const {
+            return m_patch;
+        }
+
+        constexpr std::strong_ordering operator<=>(VersionInfo const& other) const {
+            return std::tie(m_major, m_minor, m_patch) <=>
+                std::tie(other.m_major, other.m_minor, other.m_patch);
+        }
 
         static bool validate(std::string const& string);
 
         std::string toString() const;
     };
-
     void GEODE_DLL to_json(nlohmann::json& json, VersionInfo const& info);
 
-    inline std::ostream& operator<<(std::ostream& stream, VersionInfo const& version) {
-        stream << version.toString();
-        return stream;
-    }
+    std::ostream& GEODE_DLL operator<<(std::ostream& stream, VersionInfo const& version);
 }
--- loader/src/utils/VersionInfo.cpp
+++ loader/src/utils/VersionInfo.cpp
@@ -13,12 +13,6 @@ USE_GEODE_NAMESPACE();
     #define GEODE_SSCANF sscanf
 #endif
 
-VersionInfo::VersionInfo(char const* versionString) {
-    if (!GEODE_SSCANF(versionString, "v%d.%d.%d", &this->m_major, &this->m_minor, &this->m_patch)) {
-        GEODE_SSCANF(versionString, "%d.%d.%d", &this->m_major, &this->m_minor, &this->m_patch);
-    }
-}
-
 bool VersionInfo::validate(std::string const& string) {
     int buf0, buf1, buf2;
     if (GEODE_SSCANF(string.c_str(), "v%d.%d.%d", &buf0, &buf1, &buf2)) return true;
@@ -26,89 +20,21 @@ bool VersionInfo::validate(std::string const& string) {
     return false;
 }
 
-VersionInfo::VersionInfo(std::string const& versionString) : VersionInfo(versionString.c_str()) {}
-
-int VersionInfo::getMajor() const {
-    return this->m_major;
-}
-
-int VersionInfo::getMinor() const {
-    return this->m_minor;
-}
-
-int VersionInfo::getPatch() const {
-    return this->m_patch;
-}
-
-bool VersionInfo::operator==(VersionInfo const& other) const {
-    return this->match(other);
-}
-
-bool VersionInfo::operator<(VersionInfo const& other) const {
-    return std::tie(m_major, m_minor, m_patch) <
-        std::tie(other.m_major, other.m_minor, other.m_patch);
-}
-
-bool VersionInfo::operator<=(VersionInfo const& other) const {
-    return std::tie(m_major, m_minor, m_patch) <=
-        std::tie(other.m_major, other.m_minor, other.m_patch);
-}
-
-bool VersionInfo::operator>(VersionInfo const& other) const {
-    return std::tie(m_major, m_minor, m_patch) >
-        std::tie(other.m_major, other.m_minor, other.m_patch);
-}
-
-bool VersionInfo::operator>=(VersionInfo const& other) const {
-    return std::tie(m_major, m_minor, m_patch) >=
-        std::tie(other.m_major, other.m_minor, other.m_patch);
-}
-
-bool VersionInfo::match(VersionInfo const& other) const {
-    return other.m_major == this->m_major && other.m_minor == this->m_minor &&
-        other.m_patch == this->m_patch;
-}
-
-bool VersionInfo::match(VersionInfo const& other, VersionInfo::Compare compare) const {
-    switch (compare) {
-        case VersionInfo::Exact: return (*this) == other;
-        case VersionInfo::Lower: return (*this) <= other;
-        case VersionInfo::Higher: return (*this) >= other;
-        default: break;
+VersionInfo::VersionInfo(std::string const& versionString) {
+    if (!GEODE_SSCANF(versionString.c_str(), "v%d.%d.%d", &m_major, &m_minor, &m_patch)) {
+        GEODE_SSCANF(versionString.c_str(), "%d.%d.%d", &m_major, &m_minor, &m_patch);
     }
-    return false;
-}
-
-bool VersionInfo::match(
-    VersionInfo const& other, VersionInfo::Compare major, VersionInfo::Compare minor,
-    VersionInfo::Compare patch
-) const {
-    switch (major) {
-        case VersionInfo::Exact: return this->m_major == other.m_major;
-        case VersionInfo::Lower: return this->m_major <= other.m_major;
-        case VersionInfo::Higher: return this->m_major >= other.m_major;
-        case VersionInfo::Any: return true;
-    }
-    switch (minor) {
-        case VersionInfo::Exact: return this->m_minor == other.m_minor;
-        case VersionInfo::Lower: return this->m_minor <= other.m_minor;
-        case VersionInfo::Higher: return this->m_minor >= other.m_minor;
-        case VersionInfo::Any: return true;
-    }
-    switch (patch) {
-        case VersionInfo::Exact: return this->m_patch == other.m_patch;
-        case VersionInfo::Lower: return this->m_patch <= other.m_patch;
-        case VersionInfo::Higher: return this->m_patch >= other.m_patch;
-        case VersionInfo::Any: return true;
-    }
-    return false;
-}
-
-void to_json(nlohmann::json& json, VersionInfo const& info) {
-    json = info.toString();
 }
 
 std::string VersionInfo::toString() const {
-    return "v" + std::to_string(this->m_major) + "." + std::to_string(this->m_minor) + "." +
-        std::to_string(this->m_patch);
+    return fmt::format("v{}.{}.{}", m_major, m_minor, m_patch);
+}
+
+void geode::to_json(nlohmann::json& json, VersionInfo const& info) {
+    json = info.toString();
+}
+
+std::ostream& geode::operator<<(std::ostream& stream, VersionInfo const& version) {
+    stream << version.toString();
+    return stream;
 }
