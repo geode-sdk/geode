9465d7ff75e9ec46774cac78e8751bc19132fe48
camila314 <47485054+camila314@users.noreply.github.com>
2022-12-10 23:10:52 -0600

    UILayer id

---
+++ loader/src/ids/UILayer.cpp
@@ -0,0 +1,34 @@
+#include <Geode/Modify.hpp>
+#include <Geode/Bindings.hpp>
+#include <Geode/utils/cocos.hpp>
+#include "AddIDs.hpp"
+
+USE_GEODE_NAMESPACE();
+
+$register_ids(UILayer) {
+	if (auto menu = getChildOfType<CCMenu>(this, 0)) {
+		menu->setID("pause-button-menu");
+
+		setIDs(menu, 0, "pause-button");
+	}
+
+	if (auto menu = getChildOfType<CCMenu>(this, 1)) {
+		menu->setID("checkpoint-menu");
+
+		setIDs(menu, 0,
+			"add-checkpoint-button",
+			"remove-checkpoint-button"
+		);
+	}
+}
+
+class $modify(UILayer) {
+    bool init() {
+        if (!UILayer::init())
+            return false;
+        
+        NodeIDs::get()->provide(this);
+
+        return true;
+    }
+};
