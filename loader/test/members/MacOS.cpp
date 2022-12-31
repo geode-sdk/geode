fab8c1eae3cf222404e3c9d0c6f977f6a0d18dd3
altalk23 <45172705+altalk23@users.noreply.github.com>
2022-12-31 16:06:05 +0300

    add some generic member tests to macos

--- loader/test/members/MacOS.cpp
+++ loader/test/members/MacOS.cpp
@@ -8,4 +8,18 @@ GEODE_MEMBER_CHECK(PlayerObject, m_particleSystems, 0x590);
 GEODE_MEMBER_CHECK(PlayerObject, m_waveTrail, 0x600);
 GEODE_MEMBER_CHECK(PlayerObject, m_lastCollidedSolid, 0x690);
 GEODE_MEMBER_CHECK(PlayerObject, m_playerColor1, 0x7c2);
+
+// EditorUI
+GEODE_MEMBER_CHECK(EditorUI, m_buttonBar, 0x198);
+GEODE_MEMBER_CHECK(EditorUI, m_selectedObject, 0x440);
+
+// LevelEditorLayer
+GEODE_MEMBER_CHECK(LevelEditorLayer, m_editorUI, 0x5d8);
+GEODE_MEMBER_CHECK(LevelEditorLayer, m_level, 0x618);
+
+// GameManager
+GEODE_MEMBER_CHECK(GameManager, m_playLayer, 0x180);
+GEODE_MEMBER_CHECK(GameManager, m_levelEditorLayer, 0x188);
+// GEODE_MEMBER_CHECK(GameManager, m_clickedGarage, 0x2a2); // todo: find why this is not 0x2a2
+
 #endif
