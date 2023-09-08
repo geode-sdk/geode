b3d444a0f3b840f29604e8d4176ad21b1b5aeb91
altalk23 <45172705+altalk23@users.noreply.github.com>
2023-09-08 15:25:55 +0300

    Keep scroll value when refreshing the list

--- loader/src/ui/internal/list/ModListLayer.cpp
+++ loader/src/ui/internal/list/ModListLayer.cpp
@@ -444,11 +444,16 @@ void ModListLayer::createSearchControl() {
     this->addChild(m_searchInput);
 }
 
-void ModListLayer::reloadList(std::optional<ModListQuery> const& query) {
+void ModListLayer::reloadList(bool keepScroll, std::optional<ModListQuery> const& query) {
     auto winSize = CCDirector::sharedDirector()->getWinSize();
 
     if (query) {
         m_query = query.value();
+    }
+
+    float scroll = 0.0f;
+    if (keepScroll && m_list) {
+        scroll = m_list->m_listView->m_tableView->m_contentLayer->getPositionY();
     }
 
     // set search query
@@ -487,6 +492,12 @@ void ModListLayer::reloadList(std::optional<ModListQuery> const& query) {
         m_listLabel->setString("No mods found");
     } else {
         m_listLabel->setVisible(false);
+    }
+
+    if (keepScroll) {
+        list->m_tableView->m_contentLayer->setPosition(
+            { 0.0f, scroll }
+        );
     }
 
     // update index if needed
@@ -686,7 +697,7 @@ void ModListLayer::keyDown(enumKeyCodes key) {
 }
 
 void ModListLayer::textChanged(CCTextInputNode* input) {
-    this->reloadList();
+    this->reloadList(false);
 }
 
 // Constructors etc.
