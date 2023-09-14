175f488c23202079f190e57ae88f7ceff71622ca
altalk23 <45172705+altalk23@users.noreply.github.com>
2023-09-14 14:50:13 +0300

    Add android.cpp to test members

---
+++ loader/test/members/Android.cpp
@@ -0,0 +1,9 @@
+#include "Common.hpp"
+
+#ifdef GEODE_IS_ANDROID
+
+// Add known android struct members here
+
+// needed classes are ones in the ids folder and some generic ones (i think they are already done though so only ids)
+
+#endif
