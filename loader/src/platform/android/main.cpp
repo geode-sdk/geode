e7e5ee7f26d109373861eadcb34a04459c3d922e
matcool <26722564+matcool@users.noreply.github.com>
2023-09-17 15:01:34 -0300

    add some opengl functions here, for now

--- loader/src/platform/android/main.cpp
+++ loader/src/platform/android/main.cpp
@@ -5,7 +5,17 @@
 #include "../load.hpp"
 #include <jni.h>
 
+// idk where to put this
+#include <EGL/egl.h>
+PFNGLGENVERTEXARRAYSOESPROC glGenVertexArraysOESEXT = 0;
+PFNGLBINDVERTEXARRAYOESPROC glBindVertexArrayOESEXT = 0;
+PFNGLDELETEVERTEXARRAYSOESPROC glDeleteVertexArraysOESEXT = 0;
+
 extern "C" [[gnu::visibility("default")]] jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+    glGenVertexArraysOESEXT = (PFNGLGENVERTEXARRAYSOESPROC)eglGetProcAddress("glGenVertexArraysOES");
+    glBindVertexArrayOESEXT = (PFNGLBINDVERTEXARRAYOESPROC)eglGetProcAddress("glBindVertexArrayOES");
+    glDeleteVertexArraysOESEXT = (PFNGLDELETEVERTEXARRAYSOESPROC)eglGetProcAddress("glDeleteVertexArraysOES");
+
     geodeEntry(nullptr);
     return JNI_VERSION_1_1;
 }
