877b6449626078783abde9cb13ecb04e75c1ea74
altalk23 <45172705+altalk23@users.noreply.github.com>
2023-10-01 23:58:16 +0300

    fix ccmenuitemsprite virtuals

--- loader/include/Geode/platform/android.hpp
+++ loader/include/Geode/platform/android.hpp
@@ -2,6 +2,8 @@
 
 #include <dlfcn.h>
 
+#include "ItaniumCast.hpp"
+
 namespace geode {
     struct PlatformInfo {
         void* m_so;
@@ -10,12 +12,4 @@ namespace geode {
 
 namespace geode::base {
     /*GEODE_NOINLINE inline*/ uintptr_t get();
-}
-
-namespace geode::cast {
-    template <class After, class Before>
-    After typeinfo_cast(Before ptr) {
-        // yall have symbols smh
-        return dynamic_cast<After>(ptr);
-    }
 }
