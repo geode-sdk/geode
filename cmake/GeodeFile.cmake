0a113744821b2a79d30b54e4b137bd36a0cbd752
matcool <26722564+matcool@users.noreply.github.com>
2023-10-08 16:02:22 -0300

    fix GEODE_CLI force caching when not found

--- cmake/GeodeFile.cmake
+++ cmake/GeodeFile.cmake
@@ -1,7 +1,7 @@
 set(GEODE_CLI_MINIMUM_VERSION 1.0.5)
 
 # Find Geode CLI
-if (NOT DEFINED GEODE_CLI)
+if (NOT DEFINED GEODE_CLI OR GEODE_CLI STREQUAL "GEODE_CLI-NOTFOUND")
     find_program(GEODE_CLI NAMES geode.exe geode-cli.exe geode geode-cli PATHS ${CLI_PATH})
 endif()
 
