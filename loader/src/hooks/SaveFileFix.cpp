0787b8f450d421d9c81f0782a1d70ec8128b9266
matcool <26722564+matcool@users.noreply.github.com>
2023-12-17 22:33:32 -0300

    message for a bug i cant recreate

--- loader/src/hooks/SaveFileFix.cpp
+++ loader/src/hooks/SaveFileFix.cpp
@@ -5,6 +5,17 @@
 using namespace geode::prelude;
 
 #include <Geode/cocos/support/base64.h>
+#include "../loader/LoaderImpl.hpp"
+
+void panic(std::string reason) {
+    LoaderImpl::get()->platformMessageBox("Critical", fmt::format(
+        "Your save file failed to load (reason: {})\n"
+        "As to not lose all of your data, the game will now abort.\n"
+        "Please backup your save files and try opening the game again, it might work.\n"
+        "Please contact the Geode Team about this", reason
+    ));
+    std::abort();
+}
 
 // This function is well known for crashing on certain save files,
 // causing the game to crash at startup, known as the infamous save file bug.
@@ -14,23 +25,26 @@ using namespace geode::prelude;
 // 
 // To fix this, we just rewrite the function.
 gd::string decompressString2(unsigned char* data, bool decrypt, int size, int decryptionKey) {
-    if (data == nullptr || size == 0 || *data == 0) {
+    log::debug("decompressString2 data={} size={}", reinterpret_cast<const void*>(data), size);
+    if (data == nullptr || size == 0) {
         return {};
     }
 
+    std::vector<unsigned char> copiedData(data, data + size);
     if (decrypt) {
         for (int i = 0; i < size; i++) {
-            data[i] ^= decryptionKey;
+            copiedData[i] ^= decryptionKey;
         }
     }
 
     // TODO: maybe not use cocos's base64 and inflateMemory..
 
     unsigned char* out = nullptr;
-    auto const decodedSize = cocos2d::base64Decode(data, size, &out);
+    auto const decodedSize = cocos2d::base64Decode(copiedData.data(), size, &out);
     std::unique_ptr<unsigned char> b64decoded { out };
 
     if (decodedSize <= 0) {
+        panic(fmt::format("base64 (size={}) (data={} size={})", decodedSize, reinterpret_cast<const void*>(data), size));
         return {};
     }
 
@@ -40,6 +54,7 @@ gd::string decompressString2(unsigned char* data, bool decrypt, int size, int de
     std::unique_ptr<unsigned char> inflated { out };
 
     if (inflatedSize <= 0) {
+        panic(fmt::format("inflate (size={}) (data={} size={})", inflatedSize, reinterpret_cast<const void*>(data), size));
         return {};
     }
 
