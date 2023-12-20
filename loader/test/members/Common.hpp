7c0d7ba57a2038af5b96a367679aeb70c045a12d
altalk23 <45172705+altalk23@users.noreply.github.com>
2023-12-20 18:36:54 +0300

    make member check and size check less confusing

--- loader/test/members/Common.hpp
+++ loader/test/members/Common.hpp
@@ -6,19 +6,36 @@ struct False {
     static constexpr auto value = false;
 };
 
-template<int Difference, class Class, class Member, bool Predicate>
+template<int Difference, int Expected, int Current, class Class, class Member, bool Predicate>
 struct MemberIsOffsetBy {
-    static_assert(False<Difference>::value, "Member is offset by this value");
+    static_assert(False<Difference, Expected, Current>::value, "Member is offset by this value (Difference, Expected, Current)");
 };
 
-template<int Difference, class Class, class Member>
+template<int Difference, int Expected, int Current, class Class, bool Predicate>
+struct SizeIsOffsetBy {
+    static_assert(False<Difference, Expected, Current>::value, "Size is offset by this value (Difference, Expected, Current)");
+};
+
+template<int Difference, int Expected, int Current, class Class, class Member>
 struct MemberIsOffsetBy<Difference, Class, Member, true> {
     
 };
 
-template <class Member, class Class, int Offset, int Expected>
-struct SingleChecker {
-    using IsOffsetBy = MemberIsOffsetBy<Offset - Expected, Class, Member, Expected == Offset>;
+template<int Difference, int Expected, int Current, class Class>
+struct SizeIsOffsetBy<Difference, Class, true> {
+    
 };
 
-#define GEODE_MEMBER_CHECK(Class_, Member_, Offset_) class Member_; SingleChecker<Member_, Class_, offsetof(Class_, Member_), Offset_>::IsOffsetBy GEODE_CONCAT(OffsetBy, __LINE__)
+template <class Member, class Class, int Offset, int Expected>
+struct SingleMemberChecker {
+    using IsOffsetBy = MemberIsOffsetBy<Offset - Expected, Expected, Offset, Class, Member, Expected == Offset>;
+};
+
+template <class Class, int Offset, int Expected>
+struct SingleSizeChecker {
+    using IsOffsetBy = MemberIsOffsetBy<Offset - Expected, Expected, Offset, Class, Member, Expected == Offset>;
+};
+
+#define GEODE_MEMBER_CHECK(Class_, Member_, Offset_) class Member_; SingleMemberChecker<Member_, Class_, offsetof(Class_, Member_), Offset_>::IsOffsetBy GEODE_CONCAT(OffsetBy, __LINE__)
+
+#define GEODE_SIZE_CHECK(Class_, Offset_) class Class_; SingleMemberChecker<Class_, sizeof(Class_), Offset_>::IsOffsetBy GEODE_CONCAT(OffsetBy, __LINE__)
