a8e53426e35286367d5258cfcfdb6e6a5eaa4bdc
altalk23 <45172705+altalk23@users.noreply.github.com>
2024-01-01 20:46:13 +0300

    fix touch dispatcher

--- loader/src/hooks/TouchDispatcherFix.cpp
+++ loader/src/hooks/TouchDispatcherFix.cpp
@@ -3,11 +3,10 @@ using namespace geode::prelude;
 #include <Geode/modify/CCTouchDispatcher.hpp>
 
 struct ForcePrioRevert : Modify<ForcePrioRevert, CCTouchDispatcher> {
-    bool isUsingForcePrio() {
-        return true;
-    }
-
     void addTargetedDelegate(CCTouchDelegate* delegate, int priority, bool swallowsTouches) {
+        m_targetPrio = 0xb00b5; // fuck you windows i hate you for inlining isUsingForcePrio
+
+        log::debug("adding targeted delegate from {}", dynamic_cast<CCObject*>(delegate));
         m_forcePrio = -2;
         if (m_pTargetedHandlers->count() > 0) {
             auto handler = static_cast<CCTouchHandler*>(m_pTargetedHandlers->objectAtIndex(0));
