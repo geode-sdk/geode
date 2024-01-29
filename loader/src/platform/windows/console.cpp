0af02fd36de15245c9e68fd131d5970bf36db3db
ConfiG <cgytrus@cgyt.ru>
2024-01-29 21:14:13 +0300

    redirect stdout/stderr to logger

--- loader/src/platform/windows/console.cpp
+++ loader/src/platform/windows/console.cpp
@@ -1,15 +1,66 @@
 ﻿#include <loader/console.hpp>
 #include <loader/LogImpl.hpp>
+#include <io.h>
 
 using namespace geode::prelude;
 
 HANDLE s_outHandle = nullptr;
+FILE* s_origStdOut = nullptr;
 bool s_hasAnsiColorSupport = false;
 bool s_shouldAlloc = false;
 
+bool redirectStd(FILE* which, std::string const& name, const Severity sev) {
+    auto pipeName = fmt::format(R"(\\.\pipe\geode-{}-{})", name, GetCurrentProcessId());
+    auto pipe = CreateNamedPipeA(
+        pipeName.c_str(),
+        PIPE_ACCESS_INBOUND,
+        PIPE_TYPE_BYTE | PIPE_READMODE_BYTE | PIPE_WAIT | PIPE_REJECT_REMOTE_CLIENTS,
+        1, 0, 1024, 0, nullptr
+    );
+    if (!pipe) {
+        log::warn("Failed to create pipe, {} will be unavailable", name);
+        return false;
+    }
+    std::thread([pipe, name, sev]() {
+        thread::setName(fmt::format("{} Read Thread", name));
+        std::string cur;
+        while (true) {
+            char buf[1024];
+            DWORD read = 0;
+            if (!ReadFile(pipe, buf, 1024, &read, nullptr))
+                continue;
+            for (auto i = 0; i < read; i++) {
+                if (buf[i] != '\n') {
+                    if (buf[i] != '\r')
+                        cur += buf[i];
+                    continue;
+                }
+                log::Logger::get()->push(sev, "", std::string(name), 0, std::string(cur));
+                cur.clear();
+            }
+        }
+    }).detach();
+    FILE* yum;
+    if (freopen_s(&yum, pipeName.c_str(), "w", which)) {
+        log::warn("Failed to reopen file, {} will be unavailable", name);
+        return false;
+    }
+    return true;
+}
+
 void console::setup() {
+    bool hasStdout = _fileno(stdout) >= 0;
+    if (!s_shouldAlloc) {
+        s_origStdOut = hasStdout ? _fdopen(_dup(_fileno(stdout)), "w") : nullptr;
+        if (!redirectStd(stdout, "stdout", Severity::Info) && s_origStdOut) {
+            fclose(s_origStdOut);
+            s_origStdOut = nullptr;
+        }
+        redirectStd(stderr, "stderr", Severity::Debug);
+    }
+
     s_outHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-    if (!s_outHandle) {
+    if (!s_outHandle || !hasStdout) {
         if (s_shouldAlloc)
             return;
         if (!AttachConsole(ATTACH_PARENT_PROCESS)) {
@@ -17,7 +68,7 @@ void console::setup() {
             return;
         }
         s_outHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-        if (!s_outHandle) {
+        if (!s_outHandle || !hasStdout) {
             s_shouldAlloc = true;
             return;
         }
@@ -65,6 +116,11 @@ void console::log(std::string const& msg, Severity severity) {
     DWORD written;
 
     if (!s_hasAnsiColorSupport) {
+        if (s_origStdOut) {
+            fwrite(msg.c_str(), 1, msg.size(), s_origStdOut);
+            fwrite("\n", 1, 1, s_origStdOut);
+            fflush(s_origStdOut);
+        }
         WriteConsoleA(s_outHandle, msg.c_str(), msg.size(), &written, nullptr);
         WriteConsoleA(s_outHandle, "\n", 1, &written, nullptr);
         return;
@@ -99,6 +155,11 @@ void console::log(std::string const& msg, Severity severity) {
         "{}{}{}{}\x1b[0m\n",
         colorStr, msg.substr(0, 14), color2Str, msg.substr(14)
     );
+
+    if (s_origStdOut) {
+        fwrite(newMsg.c_str(), 1, newMsg.size(), s_origStdOut);
+        fflush(s_origStdOut);
+    }
     WriteConsoleA(s_outHandle, newMsg.c_str(), newMsg.size(), &written, nullptr);
 }
 
