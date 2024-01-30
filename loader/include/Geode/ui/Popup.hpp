2fab7147f15e93cbaabc0c40e75f061e3c1f94e6
altalk23 <45172705+altalk23@users.noreply.github.com>
2024-01-30 17:26:04 +0300

    rob on his way to implement the most unintuitive system ever known to mankind

--- loader/include/Geode/ui/Popup.hpp
+++ loader/include/Geode/ui/Popup.hpp
@@ -18,12 +18,18 @@ namespace geode {
             cocos2d::CCTouchDispatcher::get()->unregisterForcePrio(this);
         }
 
+        void registerWithTouchDispatcher() override {
+            cocos2d::CCTouchDispatcher::get()->addTargetedDelegate(this, -500, true);
+        }
+
         bool init(
             float width, float height, InitArgs... args, char const* bg = "GJ_square01.png",
             cocos2d::CCRect bgRect = { 0, 0, 80, 80 }
         ) {
-            auto winSize = cocos2d::CCDirector::sharedDirector()->getWinSize();
+            auto winSize = cocos2d::CCDirector::get()->getWinSize();
             m_size = cocos2d::CCSize { width, height };
+
+            cocos2d::CCTouchDispatcher::get()->registerForcePrio(this, 2);
 
             if (!this->initWithColor({ 0, 0, 0, 105 })) return false;
             m_mainLayer = cocos2d::CCLayer::create();
@@ -55,10 +61,6 @@ namespace geode {
 
             this->setKeypadEnabled(true);
             this->setTouchEnabled(true);
-
-            cocos2d::CCTouchDispatcher::get()->registerForcePrio(this, 2);
-
-            cocos::handleTouchPriority(this);
 
             return true;
         }
