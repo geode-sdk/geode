96fee440645882db6c6fa92509936ed74aa1b6ea
Cvolton <cvolton@cvolton.eu>
2024-03-11 00:37:24 +0100

    fix impostor playlayer fix being enabled in forward compat

--- loader/src/hooks/ImpostorPlayLayerFix.cpp
+++ loader/src/hooks/ImpostorPlayLayerFix.cpp
@@ -10,6 +10,8 @@ using namespace geode::prelude;
 // This workaround solves the issue by making it impossible to start the level during a transition.
 
 struct CustomLevelPage : Modify<CustomLevelPage, LevelPage> {
+    GEODE_FORWARD_COMPAT_DISABLE_HOOKS("Impostor PlayLayer fix")
+
     void onPlay(cocos2d::CCObject* sender) {
         if (!typeinfo_cast<CCTransitionScene*>(CCScene::get())) {
             LevelPage::onPlay(sender);
