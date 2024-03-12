e0993c18b8f87cbcb9f439d1be7f165fb6b2da45
cloudskies <113025487+cloudskies13@users.noreply.github.com>
2024-03-12 14:10:47 +0300

    remove comments

--- loader/src/hooks/ImpostorPlayLayerFix.cpp
+++ loader/src/hooks/ImpostorPlayLayerFix.cpp
@@ -1,19 +1,16 @@
 #include <Geode/DefaultInclude.hpp>
 #include <Geode/modify/LevelPage.hpp>
 
-using namespace geode::prelude;
+using geode::prelude::LevelPage;
+using cocos2d::CCObject;
+using cocos2d::CCScene;
+using cocos2d::CCTransitionScene;
 
-// When pressing spacebar during the transition into LevelSelectLayer,
-// a PlayLayer will be created but it will never be added to a scene.
-// It will still exist in memory, potentially causing issues in mods, i.e. Custom Keybinds.
-//
-// This workaround solves the issue by making it impossible to start the level during a transition.
-
-struct CustomLevelPage : Modify<CustomLevelPage, LevelPage> {
+struct CustomLevelPage : geode::modify::Modify<CustomLevelPage, LevelPage> {
     GEODE_FORWARD_COMPAT_DISABLE_HOOKS("Impostor PlayLayer fix")
 
-    void onPlay(cocos2d::CCObject* sender) {
-        if (!typeinfo_cast<CCTransitionScene*>(CCScene::get())) {
+    void onPlay(CCObject* sender) override {
+        if (!dynamic_cast<CCTransitionScene*>(CCScene::get())) {
             LevelPage::onPlay(sender);
         }
     }
