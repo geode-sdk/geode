26e055505740478dc0fff43e247b52463ab38219
cloudskies <113025487+cloudskies13@users.noreply.github.com>
2024-03-12 14:38:59 +0300

    uncomment

--- loader/src/hooks/TextInputNodeFix.cpp
+++ loader/src/hooks/TextInputNodeFix.cpp
@@ -4,17 +4,17 @@
 
 using namespace geode::prelude;
 
-// TODO: commenting cause cctextinputnode is changed enough that i'm not sure how it works now
-
-// struct TextNodeFix : Modify<TextNodeFix, CCTextInputNode> {
-// 	bool onTextFieldInsertText(cocos2d::CCTextFieldTTF* field, char const* text, int count) {
-//         auto change = count >= m_maxLabelLength ? 1 : 0;
-
-//         m_maxLabelLength += change;
-// 		auto ret = CCTextInputNode::onTextFieldInsertText(field, text, count);
-//         m_maxLabelLength -= change;
-//         return ret;
-// 	}
-// };
+struct TextNodeFix : Modify<TextNodeFix, CCTextInputNode> {
+    bool onTextFieldInsertText(cocos2d::CCTextFieldTTF* field, const char* text, int count) override {
+        if (count >= m_maxLabelLength) {
+            ++m_maxLabelLength;
+            bool ret = CCTextInputNode::onTextFieldInsertText(field, text, count);
+            --m_maxLabelLength;
+            return ret;
+        } else {
+            return CCTextInputNode::onTextFieldInsertText(field, text, count);
+        }
+    }
+};
 
 #endif
