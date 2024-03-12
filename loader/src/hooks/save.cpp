aebdee2cd35133cdff13c0641953f79aaa6201d5
cloudskies <113025487+cloudskies13@users.noreply.github.com>
2024-03-12 14:08:48 +0300

    use chrono

--- loader/src/hooks/save.cpp
+++ loader/src/hooks/save.cpp
@@ -1,22 +1,22 @@
+#include <chrono>
 #include <Geode/loader/Loader.hpp>
-
-using namespace geode::prelude;
-
 #include <Geode/modify/AppDelegate.hpp>
 #include <Geode/modify/CCApplication.hpp>
+
+using namespace geode::prelude;
 
 namespace {
     void saveModData() {
         log::info("Saving mod data...");
         log::pushNest();
 
-        auto begin = std::chrono::high_resolution_clock::now();
+        auto startTime = std::chrono::high_resolution_clock::now();
 
-        (void)Loader::get()->saveData();
+        Loader::get()->saveData();
 
-        auto end = std::chrono::high_resolution_clock::now();
-        auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
-        log::info("Took {}s", static_cast<float>(time) / 1000.f);
+        auto endTime = std::chrono::high_resolution_clock::now();
+        auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+        log::info("Took {}s", static_cast<float>(elapsedTime) / 1000.f);
 
         log::popNest();
     }
@@ -26,7 +26,7 @@ struct SaveLoader : Modify<SaveLoader, AppDelegate> {
     GEODE_FORWARD_COMPAT_DISABLE_HOOKS("save moved to CCApplication::gameDidSave()")
     void trySaveGame(bool p0) {
         saveModData();
-        return AppDelegate::trySaveGame(p0);
+        AppDelegate::trySaveGame(p0);
     }
 };
 
@@ -36,7 +36,7 @@ struct FallbackSaveLoader : Modify<FallbackSaveLoader, CCApplication> {
     GEODE_FORWARD_COMPAT_ENABLE_HOOKS("")
     void gameDidSave() {
         saveModData();
-        return CCApplication::gameDidSave();
+        CCApplication::gameDidSave();
     }
 };
 
@@ -47,7 +47,6 @@ struct FallbackSaveLoader : Modify<FallbackSaveLoader, CCApplication> {
 #include <Geode/modify/FileOperation.hpp>
 #include <Geode/loader/Dirs.hpp>
 
-// redirects the save path to what geode knows, in case launcher's fopen hook fails
 struct FileOperationOverride : Modify<FileOperationOverride, FileOperation> {
     gd::string getFilePath() {
         return dirs::getSaveDir().string() + "/";
