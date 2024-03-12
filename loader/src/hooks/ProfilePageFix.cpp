60a9dbf2791b9338b22ffd104422f408edbf5116
cloudskies <113025487+cloudskies13@users.noreply.github.com>
2024-03-12 14:37:26 +0300

    optimize

--- loader/src/hooks/ProfilePageFix.cpp
+++ loader/src/hooks/ProfilePageFix.cpp
@@ -3,13 +3,12 @@
 
 using namespace geode::prelude;
 
-// Fixes the names not showing up on profile links
 struct CustomProfilePage : Modify<CustomProfilePage, ProfilePage> {
-    GEODE_FORWARD_COMPAT_DISABLE_HOOKS("ProfilePage fix")
+    GEODE_FORWARD_COMPAT_DISABLE_HOOKS("ProfilePage Fix")
 
-    virtual TodoReturn getUserInfoFinished(GJUserScore* info) {
-        m_usernameLabel->setString(info->m_userName.c_str());
-        m_usernameLabel->limitLabelWidth(info->m_modBadge > 0 ? 140.f : 160.0f, 0.8f, 0.0f);
-        ProfilePage::getUserInfoFinished(info);
+    TodoReturn getUserInfoFinished(GJUserScore* info) override {
+        m_usernameLabel->setString(info->m_userName);
+        m_usernameLabel->limitLabelWidth(info->m_modBadge > 0 ? 140.0f : 160.0f, 0.8f, 0.0f);
+        return ProfilePage::getUserInfoFinished(info);
     }
 };
