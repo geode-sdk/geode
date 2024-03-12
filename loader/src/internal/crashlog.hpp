b40afc3391c510d3b8114f91badb9800037ddedf
cloudskies <113025487+cloudskies13@users.noreply.github.com>
2024-03-12 14:51:11 +0300

    better

--- loader/src/internal/crashlog.hpp
+++ loader/src/internal/crashlog.hpp
@@ -1,42 +1,17 @@
 #pragma once
 
-#include <Geode/Loader.hpp>
 #include <ghc/fs_fwd.hpp>
 #include <string>
+#include <sstream>
+#include <Geode/Loader.hpp>
 
-/**
- * Platform-specific crashlog functions. Used by the loader
- */
 namespace crashlog {
-    /**
-     * Setup platform-specific crashlog handler
-     * @returns True if the handler was successfully installed, false otherwise
-     */
-    bool GEODE_DLL setupPlatformHandler();
-
-    /**
-     * Setup platform-specific crashlog handler for post-launch
-     */
-    void GEODE_DLL setupPlatformHandlerPost();
-
-    /**
-     * Check if previous launch of GD crashed unexpectedly
-     * @returns True if the launch crashed, false otherwise or if indeterminate
-     */
-    bool GEODE_DLL didLastLaunchCrash();
-    /**
-     * Get the directory where crashlogs are saved on the current platform
-     * @returns Path to the directory, or an empty string if the platform does
-     * not support crash logs
-     */
-    ghc::filesystem::path GEODE_DLL getCrashLogDirectory();
-
-    std::string GEODE_DLL writeCrashlog(geode::Mod* faultyMod, std::string const& info, std::string const& stacktrace, std::string const& registers);
-
+    bool setupPlatformHandler();
+    void setupPlatformHandlerPost();
+    bool didLastLaunchCrash();
+    ghc::filesystem::path getCrashLogDirectory();
+    std::string writeCrashlog(geode::Mod* faultyMod, const std::string& info, const std::string& stacktrace, const std::string& registers);
     std::string getDateString(bool filesafe);
-
     void printGeodeInfo(std::stringstream& stream);
     void printMods(std::stringstream& stream);
-
-
 }
