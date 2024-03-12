dbbbdb745fa93f67baac0052c8b45cbfe8efe1ce
cloudskies <113025487+cloudskies13@users.noreply.github.com>
2024-03-12 14:48:52 +0300

    remove comments, typo

--- loader/src/internal/crashlog.cpp
+++ loader/src/internal/crashlog.cpp
@@ -9,97 +9,81 @@ using namespace geode::prelude;
 std::string crashlog::getDateString(bool filesafe) {
     auto const now = std::time(nullptr);
     auto const tm = *std::localtime(&now);
-    std::ostringstream oss;
+    char buffer[20];
     if (filesafe) {
-        oss << std::put_time(&tm, "%F_%H-%M-%S");
+        std::strftime(buffer, sizeof(buffer), "%F_%H-%M-%S", &tm);
     }
     else {
-        oss << std::put_time(&tm, "%FT%T%z"); // ISO 8601
+        std::strftime(buffer, sizeof(buffer), "%FT%T%z", &tm);
     }
-    return oss.str();
+    return buffer;
 }
 
 void crashlog::printGeodeInfo(std::stringstream& stream) {
-    stream << "Loader Version: " << Loader::get()->getVersion().toString() << "\n"
+    auto loader = Loader::get();
+    stream << "Loader Version: " << loader->getVersion().toString() << "\n"
            << "Loader Commit: " << about::getLoaderCommitHash() << "\n"
            << "Bindings Commit: " << about::getBindingsCommitHash() << "\n"
-           << "Installed mods: " << Loader::get()->getAllMods().size() << "\n"
-           << "Problems: " << Loader::get()->getProblems().size() << "\n";
+           << "Installed mods: " << loader->getAllMods().size() << "\n"
+           << "Problems: " << loader->getProblems().size() << "\n";
 }
 
 void crashlog::printMods(std::stringstream& stream) {
     auto mods = Loader::get()->getAllMods();
     if (mods.empty()) {
         stream << "<None>\n";
+        return;
     }
+    
     std::sort(mods.begin(), mods.end(), [](Mod* a, Mod* b) {
-        auto const s1 = a->getID();
-        auto const s2 = b->getID();
-        return std::lexicographical_compare(s1.begin(), s1.end(), s2.begin(), s2.end(), [](auto a, auto b) {
-            return std::tolower(a) < std::tolower(b);
-        });
+        return a->getID().compare(b->getID()) < 0;
     });
-    using namespace std::string_view_literals;
+    
     for (auto& mod : mods) {
-        stream << fmt::format("{} | [{}] {}\n",
-            ModImpl::getImpl(mod)->isCurrentlyLoading() ? "o"sv : 
-            mod->isEnabled() ? "x"sv : 
-            ModImpl::getImpl(mod)->hasProblems() ? "!"sv : // thank you for this bug report
-            mod->shouldLoad() ? "~"sv : 
-            " "sv,
-            mod->getVersion().toString(), mod->getID()
-        );
+        auto impl = ModImpl::getImpl(mod);
+        char status = ' ';
+        if (impl->isCurrentlyLoading()) status = 'o';
+        else if (mod->isEnabled()) status = 'x';
+        else if (impl->hasProblems()) status = '!';
+        else if (mod->shouldLoad()) status = '~';
+
+        stream << fmt::format("{} | [{}] {}\n", status, mod->getVersion().toString(), mod->getID());
     }
 }
 
-std::string crashlog::writeCrashlog(geode::Mod* faultyMod, std::string const& info, std::string const& stacktrace, std::string const& registers) {
-    // make sure crashlog directory exists
-    (void)utils::file::createDirectoryAll(crashlog::getCrashLogDirectory());
-
-    // add a file to let Geode know on next launch that it crashed previously
-    // this could also be done by saving a loader setting or smth but eh.
-    (void)utils::file::writeBinary(crashlog::getCrashLogDirectory() / "last-crashed", {});
+std::string crashlog::writeCrashlog(geode::Mod* faultyMod, const std::string& info, const std::string& stacktrace, const std::string& registers) {
+    utils::file::createDirectoryAll(crashlog::getCrashLogDirectory());
+    utils::file::writeBinary(crashlog::getCrashLogDirectory() / "last-crashed", {});
     
-    std::stringstream file;
-
-    file << getDateString(false) << "\n"
-         << std::showbase << "Whoopsies! An unhandled exception has occurred.\n";
+    std::string file;
+    file.reserve(4096);
+    
+    file += getDateString(false) + "\n";
+    file += std::showbase + "Whoopsies! An unhandled exception has occurred.\n";
 
     if (faultyMod) {
-        file << "It appears that the crash occurred while executing code from "
-             << "the \"" << faultyMod->getID() << "\" mod. "
-             << "Please submit this crash report to its developers ("
-             << ranges::join(faultyMod->getDevelopers(), ", ")
-             << ") for assistance.\n";
+        file += fmt::format("It appears that the crash occurred while executing code from the \"{}\" mod. Please submit this crash report to it's developers ({}) for assistance.\n",
+                            faultyMod->getID(), ranges::join(faultyMod->getDevelopers(), ", "));
     }
 
-    // geode info
-    file << "\n== Geode Information ==\n";
+    file += "\n== Geode Information ==\n";
     printGeodeInfo(file);
 
-    // exception info
-    file << "\n== Exception Information ==\n";
-    file << info;
+    file += "\n== Exception Information ==\n";
+    file += info;
 
-    // stack trace
-    file << "\n== Stack Trace ==\n";
-    file << stacktrace;
+    file += "\n== Stack Trace ==\n";
+    file += stacktrace;
 
-    // registers
-    file << "\n== Register States ==\n";
-    file << registers;
+    file += "\n== Register States ==\n";
+    file += registers;
 
-    // mods
-    file << "\n== Installed Mods ==\n";
+    file += "\n== Installed Mods ==\n";
     printMods(file);
 
-    // save actual file
-    std::ofstream actualFile;
-    actualFile.open(
-        crashlog::getCrashLogDirectory() / (getDateString(true) + ".log"), std::ios::app
-    );
-    actualFile << file.rdbuf() << std::flush;
+    std::ofstream actualFile((crashlog::getCrashLogDirectory() / (getDateString(true) + ".log")).string(), std::ios::app);
+    actualFile << file;
     actualFile.close();
 
-    return file.str();
+    return file;
 }
