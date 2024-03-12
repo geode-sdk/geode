2094fc6926586633f27b780d03ae5b6b0d93d30f
cloudskies <113025487+cloudskies13@users.noreply.github.com>
2024-03-12 14:12:30 +0300

    remove comments

--- loader/src/hooks/AndroidExceptionFix.cpp
+++ loader/src/hooks/AndroidExceptionFix.cpp
@@ -4,18 +4,7 @@ using namespace geode::prelude;
 
 #ifdef GEODE_IS_ANDROID32
 
-// replaces https://github.com/llvm-mirror/libcxxabi/blob/master/src/private_typeinfo.cpp#L213
-// this is the function that checks if a type can catch an exception
 bool canCatchImpl(const std::type_info* self, const std::type_info* thrown, void*& adjustedPtr) {
-    // cause who is gonna use foreign exceptions anyway
-#if 0
-    if (std::strcmp(self->name(), typeid(std::exception).name()) == 0) {
-        return true;
-    }
-    if (adjustedPtr == nullptr) {
-        return false;
-    }
-#endif
     auto vtable = reinterpret_cast<geode::cast::VtableType*>(*(intptr_t*)adjustedPtr);
     auto complete = static_cast<geode::cast::CompleteVtableType*>(vtable);
     auto typeinfo = complete->m_typeinfo;
@@ -23,13 +12,11 @@ bool canCatchImpl(const std::type_info* self, const std::type_info* thrown, void
 }
 
 $execute {
-    // we get the __class_type_info::can_catch address from the typeinfo of a dummy class
     geode::cast::DummyClass dummyClass;
-    auto vtable = reinterpret_cast<geode::cast::VtableType*>(*(intptr_t*)&dummyClass);
+    auto vtable = reinterpret_cast<geode::cast::VtableType*>(&dummyClass);
     auto complete = static_cast<geode::cast::CompleteVtableType*>(vtable);
     auto typeinfo = complete->m_typeinfo;
-    auto tiVtable = typeinfo->m_typeinfoVtable;
-    auto hookAddr = tiVtable->m_vtable[4]; // 5th entry is can_catch
+    auto hookAddr = typeinfo->m_typeinfoVtable->m_vtable[4];
 
     (void)Mod::get()->hook(hookAddr, &canCatchImpl, "__class_type_info::can_catch");
 }
