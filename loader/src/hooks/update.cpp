c2be952b2371c80eb94194117b8f30095e2dec21
cloudskies <113025487+cloudskies13@users.noreply.github.com>
2024-03-12 14:42:09 +0300

    idk??

--- loader/src/hooks/update.cpp
+++ loader/src/hooks/update.cpp
@@ -1,8 +1,8 @@
-#include <loader/LoaderImpl.hpp>
-
-using namespace geode::prelude;
-
+#include <lodaer/LoaderImpl.hpp>
 #include <Geode/modify/CCScheduler.hpp>
+
+using geode::prelude::Modify;
+using geode::prelude::CCScheduler;
 
 struct FunctionQueue : Modify<FunctionQueue, CCScheduler> {
     void update(float dt) {
