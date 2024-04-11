071b8958b0663b3863ffab8fa866fefd9f8982a8
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2024-04-11 18:40:57 +0200

    fix geode dll

--- loader/src/internal/about.hpp
+++ loader/src/internal/about.hpp
@@ -3,9 +3,9 @@
 #include <Geode/utils/VersionInfo.hpp>
 
 namespace about {
-    geode::VersionInfo GEODE_DLL getLoaderVersion();
-    const char* GEODE_DLL getLoaderVersionStr();
-    const char* GEODE_DLL getLoaderCommitHash();
-    const char* GEODE_DLL getBindingsCommitHash();
-    const char* GEODE_DLL getLoaderModJson();
+    GEODE_DLL geode::VersionInfo getLoaderVersion();
+    GEODE_DLL const char* getLoaderVersionStr();
+    GEODE_DLL const char* getLoaderCommitHash();
+    GEODE_DLL const char* getBindingsCommitHash();
+    GEODE_DLL const char* getLoaderModJson();
 };
