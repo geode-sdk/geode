798cacc1a883bd02d66489952ade3fac851a710a
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2024-04-14 00:00:17 +0200

    add a way to access internal setting container
    exposing an internal field like this isn't the best practice, but it seemed like the best and simplest solution to me to a problem of not being able to do anything to unknown setting keys (such as removing them or migrating their data if that key used to be a valid setting)

--- loader/src/loader/ModImpl.cpp
+++ loader/src/loader/ModImpl.cpp
@@ -119,6 +119,10 @@ matjson::Value& Mod::Impl::getSaveContainer() {
     return m_saved;
 }
 
+matjson::Value& Mod::Impl::getSavedSettingsData() {
+    return m_savedSettingsData;
+}
+
 bool Mod::Impl::isEnabled() const {
     return m_enabled;
 }
