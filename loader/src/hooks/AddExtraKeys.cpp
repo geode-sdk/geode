6bb9ff5beb46a0c67baa2c7275b22f91c7ee3e50
Fleeym <61891787+Fleeym@users.noreply.github.com>
2024-04-20 18:36:30 +0300

    rewrite numpad key detection

--- loader/src/hooks/AddExtraKeys.cpp
+++ loader/src/hooks/AddExtraKeys.cpp
@@ -1,14 +1,21 @@
+#include <Geode/DefaultInclude.hpp>
 #include <Geode/cocos/robtop/glfw/glfw3.h>
 #include <Geode/cocos/robtop/keyboard_dispatcher/CCKeyboardDispatcher.h>
+#include <Geode/cocos/robtop/keyboard_dispatcher/CCKeyboardDelegate.h>
 #include <Geode/cocos/text_input_node/CCIMEDispatcher.h>
 #include <Geode/modify/Modify.hpp>
 #include <Geode/modify/CCEGLView.hpp>
+#include <Geode/modify/CCKeyboardDispatcher.hpp>
 
 using namespace geode::prelude;
 
-class $modify(CCEGLView) {
+
+class $modify(GeodeCCEGLView, CCEGLView) {
     void onGLFWKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-        if (!isExtraKey(key)) {
+        log::info("glfw key {}", key);
+        bool extraKey = isExtraKey(key);
+        bool numpad = isKeyNumpad(key);
+        if (!extraKey && !numpad) {
             return CCEGLView::onGLFWKeyCallback(window, key, scancode, action, mods);
         }
         if (CCIMEDispatcher::sharedDispatcher()->hasDelegate()) {
@@ -16,7 +23,13 @@ class $modify(CCEGLView) {
         }
         bool down = action == 1 || action == 2;
         bool repeat = action == 2;
-        enumKeyCodes keyCode = this->extraKeyToKeyCode(key);
+        enumKeyCodes keyCode = enumKeyCodes::KEY_Unknown;
+        if (extraKey) {
+            keyCode = this->extraKeyToKeyCode(key);
+        }
+        if (numpad) {
+            keyCode = this->numpadToKeyCode(key);
+        }
         CCKeyboardDispatcher::get()->dispatchKeyboardMSG(keyCode, down, repeat);
     }
 
@@ -89,7 +102,7 @@ class $modify(CCEGLView) {
                 return enumKeyCodes::KEY_Slash;
             // =
             case GLFW_KEY_EQUAL:
-                return enumKeyCodes::KEY_Equal;
+                return enumKeyCodes::KEY_OEMEqual;
             // [
             case GLFW_KEY_LEFT_BRACKET:
                 return enumKeyCodes::KEY_LeftBracket;
@@ -104,6 +117,94 @@ class $modify(CCEGLView) {
                 return enumKeyCodes::KEY_GraveAccent;
             default:
                 return enumKeyCodes::KEY_Unknown;
+        }
+    }
+
+    bool isKeyNumpad(int code) {
+        return code >= GLFW_KEY_KP_0 && code <= GLFW_KEY_KP_EQUAL;
+    }
+
+    enumKeyCodes numpadToKeyCode(int key) {
+        switch (key) {
+            case GLFW_KEY_KP_0:
+                return enumKeyCodes::KEY_NumPad0;
+            case GLFW_KEY_KP_1:
+                return enumKeyCodes::KEY_NumPad1;
+            case GLFW_KEY_KP_2:
+                return enumKeyCodes::KEY_NumPad2;
+            case GLFW_KEY_KP_3:
+                return enumKeyCodes::KEY_NumPad3;
+            case GLFW_KEY_KP_4:
+                return enumKeyCodes::KEY_NumPad4;
+            case GLFW_KEY_KP_5:
+                return enumKeyCodes::KEY_NumPad5;
+            case GLFW_KEY_KP_6:
+                return enumKeyCodes::KEY_NumPad6;
+            case GLFW_KEY_KP_7:
+                return enumKeyCodes::KEY_NumPad7;
+            case GLFW_KEY_KP_8:
+                return enumKeyCodes::KEY_NumPad8;
+            case GLFW_KEY_KP_9:
+                return enumKeyCodes::KEY_NumPad9;
+            case GLFW_KEY_KP_DECIMAL:
+                return enumKeyCodes::KEY_Decimal;
+            case GLFW_KEY_KP_DIVIDE:
+                return enumKeyCodes::KEY_Divide;
+            case GLFW_KEY_KP_MULTIPLY:
+                return enumKeyCodes::KEY_Multiply;
+            case GLFW_KEY_KP_SUBTRACT:
+                return enumKeyCodes::KEY_Subtract;
+            case GLFW_KEY_KP_ADD:
+                return enumKeyCodes::KEY_Add;
+            case GLFW_KEY_KP_ENTER:
+                return enumKeyCodes::KEY_NumEnter;
+            case GLFW_KEY_KP_EQUAL:
+                return enumKeyCodes::KEY_Equal;
+            default:
+                return enumKeyCodes::KEY_Unknown;
+        }
+    }
+};
+
+class $modify(CCKeyboardDispatcher) {
+    GEODE_FORWARD_COMPAT_DISABLE_HOOKS("CCKeyboardDispatcher new keys")
+
+    const char* keyToString(enumKeyCodes key) {
+        if (key < 0x1000) {
+            return CCKeyboardDispatcher::keyToString(key);
+        }
+
+        switch (key) {
+            case KEY_GraveAccent:
+                return "`";
+            case KEY_OEMEqual:
+                return "=";
+            case KEY_LeftBracket:
+                return "[";
+            case KEY_RightBracket:
+                return "]";
+            case KEY_Backslash:
+                return "\\";
+            case KEY_Semicolon:
+                return ";";
+            case KEY_Apostrophe:
+                return "'";
+            case KEY_Slash:
+                return "/";
+            case KEY_NumEnter:
+                return "NumEnter";
+            case MOUSE_4:
+                return "Mouse 4";
+            case MOUSE_5:
+                return "Mouse 5";
+            case MOUSE_6:
+                return "Mouse 6";
+            case MOUSE_7:
+                return "Mouse 7";
+            case MOUSE_8:
+                return "Mouse 8";
+            default:
+                return CCKeyboardDispatcher::keyToString(KEY_Unknown);
         }
     }
 };
