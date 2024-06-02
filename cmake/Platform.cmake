e5ccb1bf2f9e4db1668e0bdb9f4e331d8350ac41
qimiko <25387744+qimiko@users.noreply.github.com>
2024-06-01 20:30:43 -0700

    macos was updated btw

--- cmake/Platform.cmake
+++ cmake/Platform.cmake
@@ -19,6 +19,9 @@ elseif (GEODE_TARGET_PLATFORM STREQUAL "MacOS")
 		SYSTEM_NAME MacOS
 	)
 
+	# this should be set globally
+	set(CMAKE_OSX_ARCHITECTURES "arm64;x86_64")
+
 	# only exists as a global property
 	set(CMAKE_OSX_DEPLOYMENT_TARGET 10.15)
 
