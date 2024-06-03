2971ebe569e851e68e9dc6f2738ac2bd30461f6f
The Bearodactyl <114454115+TheBearodactyl@users.noreply.github.com>
2024-06-03 15:40:47 -0500

    Update Platform.cmake

--- cmake/Platform.cmake
+++ cmake/Platform.cmake
@@ -75,6 +75,31 @@ elseif (GEODE_TARGET_PLATFORM STREQUAL "Win32")
 	set(GEODE_OUTPUT_NAME "Geode")
 	set(GEODE_PLATFORM_BINARY "Geode.lib")
 	set(GEODE_MOD_BINARY_SUFFIX ".dll" CACHE STRING "" FORCE)
+elseif(GEODE_TARGET_PLATFORM STREQUAL "Win64")
+ 	set_target_properties(${PROJECT_NAME} PROPERTIES
+		SYSTEM_NAME Win32
+		GENERATOR_PLATFORM x86
+	)
+
+ 	target_link_libraries(${PROJECT_NAME} INTERFACE 
+		${GEODE_LOADER_PATH}/include/link/win64/libcocos2d.lib
+		${GEODE_LOADER_PATH}/include/link/win64/libExtensions.lib
+		${GEODE_LOADER_PATH}/include/link/win64/ssl.lib
+		${GEODE_LOADER_PATH}/include/link/win64/crypto.lib
+		${GEODE_LOADER_PATH}/include/link/win64/nghttp2.lib
+		${GEODE_LOADER_PATH}/include/link/win64/ngtcp2.lib
+		${GEODE_LOADER_PATH}/include/link/win64/nghttp3.lib
+		${GEODE_LOADER_PATH}/include/link/win64/ngtcp2_crypto_boringssl.lib
+		${GEODE_LOADER_PATH}/include/link/win64/libcurl.lib
+		${GEODE_LOADER_PATH}/include/link/win64/glew32.lib
+		${GEODE_LOADER_PATH}/include/link/win64/gdstring.lib
+		${GEODE_LOADER_PATH}/include/link/win64/fmod.lib
+		opengl32
+	)
+ 
+ 	set(GEODE_OUTPUT_NAME "Geode")
+	set(GEODE_PLATFORM_BINARY "Geode.lib")
+	set(GEODE_MOD_BINARY_SUFFIX ".dll" CACHE STRING "" FORCE)
 elseif (GEODE_TARGET_PLATFORM STREQUAL "Android32")
 	set_target_properties(${PROJECT_NAME} PROPERTIES
 		SYSTEM_NAME Android
