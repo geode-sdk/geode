f8c7f204ae2cfb590b80efa486915f6b8a5dccde
Chloe <25387744+qimiko@users.noreply.github.com>
2024-07-13 17:32:47 -0700

    show invalid on explicit search

--- loader/src/server/Server.cpp
+++ loader/src/server/Server.cpp
@@ -566,14 +566,15 @@ ServerRequest<ServerModsList> server::getMods(ModsQuery const& query, bool useCa
     auto req = web::WebRequest();
     req.userAgent(getServerUserAgent());
 
-    // Always target current GD version and Loader version
-    req.param("gd", GEODE_GD_VERSION_STR);
-    req.param("geode", Loader::get()->getVersion().toNonVString());
-
     // Add search params
     if (query.query) {
         req.param("query", *query.query);
+    } else {
+        // Target current GD version and Loader version when query is not set
+        req.param("gd", GEODE_GD_VERSION_STR);
+        req.param("geode", Loader::get()->getVersion().toNonVString());
     }
+
     if (query.platforms.size()) {
         std::string plats = "";
         bool first = true;
