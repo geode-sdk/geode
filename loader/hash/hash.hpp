61e000daa0292477ceba1ffd171a049954d77678
matcool <26722564+matcool@users.noreply.github.com>
2024-07-20 18:16:06 -0300

    check hash for downloaded mods

--- loader/hash/hash.hpp
+++ loader/hash/hash.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <filesystem>
+#include <span>
 
 std::string calculateSHA3_256(std::filesystem::path const& path);
 
@@ -9,4 +10,8 @@ std::string calculateSHA256(std::filesystem::path const& path);
 
 std::string calculateSHA256Text(std::filesystem::path const& path);
 
-std::string calculateHash(std::filesystem::path const& path);
+/**
+ * Calculates the SHA256 hash of the given data,
+ * used for verifying mods.
+ */
+std::string calculateHash(std::span<const uint8_t> data);
