3990cde8ea00217f092d4c1a191c42d11a7cd1d6
Cvolton <cvolton@cvolton.eu>
2024-07-24 13:11:24 +0200

    add objectdecoder

---
+++ loader/include/Geode/cocos/robtop/xml/ObjectDecoder.h
@@ -0,0 +1,20 @@
+#ifndef __OBJECTDECODER_H__
+#define __OBJECTDECODER_H__
+
+#include "../../include/cocos2d.h"
+
+// @note RobTop Addition
+class CC_DLL ObjectDecoder {
+public:
+	ObjectDecoder();
+	~ObjectDecoder() {}
+    GEODE_CUSTOM_CONSTRUCTOR_BEGIN(ObjectDecoder)
+
+	static ObjectDecoder* sharedDecoder();
+
+	cocos2d::CCObject* getDecodedObject(int, DS_Dictionary*);
+
+	virtual bool init();
+};
+
+#endif
