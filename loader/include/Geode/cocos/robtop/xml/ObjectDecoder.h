509c5e7e93a8bc1159f078daf8be2e49ca0a47e9
Chloe <25387744+qimiko@users.noreply.github.com>
2024-07-25 06:58:44 -0700

    add objectdecoderdelegate for objectdecoder

--- loader/include/Geode/cocos/robtop/xml/ObjectDecoder.h
+++ loader/include/Geode/cocos/robtop/xml/ObjectDecoder.h
@@ -4,17 +4,25 @@
 #include "../../include/cocos2d.h"
 
 // @note RobTop Addition
+class CC_DLL ObjectDecoderDelegate {
+	virtual cocos2d::CCObject* getDecodedObject(int, DS_Dictionary*) { return nullptr; }
+};
+
+// @note RobTop Addition
 class CC_DLL ObjectDecoder : public cocos2d::CCNode {
 public:
 	ObjectDecoder();
 	~ObjectDecoder() {}
-    GEODE_CUSTOM_CONSTRUCTOR_BEGIN(ObjectDecoder)
+	GEODE_CUSTOM_CONSTRUCTOR_BEGIN(ObjectDecoder)
 
 	static ObjectDecoder* sharedDecoder();
 
 	cocos2d::CCObject* getDecodedObject(int, DS_Dictionary*);
 
 	virtual bool init();
+
+public:
+	ObjectDecoderDelegate* m_delegate;
 };
 
 #endif
