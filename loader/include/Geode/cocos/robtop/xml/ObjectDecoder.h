dceb91e181e408793c60bc3b69d38334e789cfa9
Chloe <25387744+qimiko@users.noreply.github.com>
2024-07-25 17:11:37 -0700

    he can go back now :D

--- loader/include/Geode/cocos/robtop/xml/ObjectDecoder.h
+++ loader/include/Geode/cocos/robtop/xml/ObjectDecoder.h
@@ -4,13 +4,10 @@
 #include "../../include/cocos2d.h"
 
 // @note RobTop Addition
-/*
-// TODO: uncomment just before 3.3.1 releases, it requires a change in bindings/codegen as well
 class CC_DLL ObjectDecoderDelegate {
 public:
-	virtual cocos2d::CCObject* getDecodedObject(int, DS_Dictionary*) { return nullptr; }
+	virtual cocos2d::CCObject* getDecodedObject(int objectType, DS_Dictionary* data) { return nullptr; }
 };
-*/
 
 // @note RobTop Addition
 class CC_DLL ObjectDecoder : public cocos2d::CCNode {
@@ -25,10 +22,8 @@ public:
 
 	virtual bool init();
 
-/*
 public:
 	ObjectDecoderDelegate* m_delegate;
-*/
 };
 
 #endif
