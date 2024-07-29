22bcbe1b5e75025f44d9e59b10b20dee708bac8e
Chloe <25387744+qimiko@users.noreply.github.com>
2024-07-29 06:18:17 -0700

    remove usages of cc3x

--- loader/include/Geode/utils/cocos.hpp
+++ loader/include/Geode/utils/cocos.hpp
@@ -861,7 +861,7 @@ namespace geode::cocos {
         return {color.r / 255.f, color.g / 255.f, color.b / 255.f, color.a / 255.f};
     }
 
-    [[deprecated("This function sucks, use cc3bFromHexString instead")]]
+    [[deprecated("This function may have unintended behavior, use cc3bFromHexString or manually expand the color instead")]]
     constexpr cocos2d::ccColor3B cc3x(int hexValue) {
         if (hexValue <= 0xf)
             return cocos2d::ccColor3B{
