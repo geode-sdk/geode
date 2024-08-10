fca774adbffdbbc7c6a2536d8af81c59d286fdea
Oleksandr Nemesh <prevter@gmail.com>
2024-08-10 13:37:08 +0300

    cleanup the header

--- loader/include/Geode/cocos/robtop/special_nodes/CCLightning.h
+++ loader/include/Geode/cocos/robtop/special_nodes/CCLightning.h
@@ -12,26 +12,17 @@ public:
     CCLightning();
     virtual ~CCLightning();
 
-    static cocos2d::CCLightning* lightningWithStrikePoint(cocos2d::CCPoint strikePoint, cocos2d::CCPoint strikePoint2, float duration);
-    static cocos2d::CCLightning* lightningWithStrikePoint(cocos2d::CCPoint strikePoint);
+    static CCLightning* lightningWithStrikePoint(CCPoint strikePoint, CCPoint strikePoint2, float duration);
+    static CCLightning* lightningWithStrikePoint(CCPoint strikePoint);
 
-    bool initWithStrikePoint(cocos2d::CCPoint strikePoint, cocos2d::CCPoint strikePoint2, float duration);
-    bool initWithStrikePoint(cocos2d::CCPoint strikePoint) {
-        return initWithStrikePoint(strikePoint, {0, 0}, 0.5f);
-    }
+    bool initWithStrikePoint(CCPoint strikePoint, CCPoint strikePoint2, float duration);
+    bool initWithStrikePoint(CCPoint strikePoint);
 
     void strike();
     void strikeFinished();
 
-    void strikeRandom() {
-        m_seed = rand();
-        this->strike();
-    }
-
-    void strikeWithSeed(uint64_t seed) {
-        m_seed = seed;
-        this->strike();
-    }
+    void strikeRandom();
+    void strikeWithSeed(uint64_t seed);
 
     void draw();
 
@@ -44,34 +35,31 @@ public:
     void updateDisplayedOpacity(unsigned char);
     bool isCascadeColorEnabled();
     void setCascadeOpacityEnabled(bool);
-    cocos2d::_ccColor3B const& getColor();
-    cocos2d::_ccColor3B const& getDisplayedColor();
-    void setColor(cocos2d::_ccColor3B const&);
-    void updateDisplayedColor(cocos2d::_ccColor3B const&);
+    ccColor3B const& getColor();
+    ccColor3B const& getDisplayedColor();
+    void setColor(ccColor3B const&);
+    void updateDisplayedColor(ccColor3B const&);
     bool isCascadeOpacityEnabled();
     void setCascadeColorEnabled(bool);
 
 protected:
-    cocos2d::CCPoint m_strikePoint;
-    cocos2d::CCPoint m_strikePoint2;
+    CCPoint m_strikePoint;
+    CCPoint m_strikePoint2;
     bool m_split;
-    PAD(7);
-    uint32_t m_displacement;
+    int m_displacement;
+    int m_minDisplacement;
     uint64_t m_seed;
     float m_lineWidth;
-    PAD(4);
+    bool m_unkBool;
     float m_duration;
     float m_extraOpacity;
-    cocos2d::CCPoint* m_lightningPoints;
+    std::array<CCPoint, 200>* m_lightningPoints;
     uint32_t m_numPoints;
     uint8_t m_unk148;
     uint8_t m_opacity;
-    PAD(3);
-    cocos2d::_ccColor3B m_tColor;
-    PAD(3);
+    ccColor3B m_tColor;
 };
 
 NS_CC_END
-
 
 #endif //__CCLIGHTNING_H__
