1af10eea7fbe71427dd2bb2e45709984fd937bee
mat <26722564+matcool@users.noreply.github.com>
2024-11-13 10:41:13 -0300

    add check for gd version in DynamicCastFix.cpp
    prevents silently breaking macos

--- loader/src/hooks/DynamicCastFix.cpp
+++ loader/src/hooks/DynamicCastFix.cpp
@@ -8,6 +8,10 @@ $execute {
     // this is needed because the transitions in cocos uses dynamic cast to check
     // layers, which fail on user layers due to typeinfo not matching
 
+    #if defined(GEODE_IS_MAC) && GEODE_COMP_GD_VERSION != 22074
+        #error "Unsupported version for macOS dynamic cast fix, please update the addresses"
+    #endif
+
     #if defined(GEODE_IS_INTEL_MAC)
         void* dynamicCastAddr = reinterpret_cast<void*>(base::get() + 0x7ba1d8);
         (void) Mod::get()->hook(dynamicCastAddr, &cast::typeinfoCastInternal, "__dynamic_cast");
