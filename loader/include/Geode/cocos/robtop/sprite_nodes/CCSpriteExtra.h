77b435e68f2e360f4d3658e8d11ced9dabadd2a5
Jasmine <52604018+hiimjustin000@users.noreply.github.com>
2024-12-17 17:14:05 -0500

    Create CCSpriteExtra.h

---
+++ loader/include/Geode/cocos/robtop/sprite_nodes/CCSpriteExtra.h
@@ -0,0 +1,26 @@
+#ifndef __CCSPRITEEXTRA_H__
+#define __CCSPRITEEXTRA_H__
+
+NS_CC_BEGIN
+
+// @note RobTop Addition
+
+class CCSpriteExtra : public CCSprite {
+public:
+    CCSpriteExtra();
+    GEODE_CUSTOM_CONSTRUCTOR_COCOS(CCSpriteExtra, CCSprite);
+
+    inline float getXOffset() const { return m_fXOffset; }
+    inline float getYOffset() const { return m_fYOffset; }
+
+    inline void setXOffset(float offset) { m_fXOffset = offset; }
+    inline void setYOffset(float offset) { m_fYOffset = offset; }
+
+protected:
+    float m_fXOffset;
+    float m_fYOffset;
+};
+
+NS_CC_END
+
+#endif // __CCSPRITEEXTRA_H__
