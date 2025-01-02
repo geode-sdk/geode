0a25ebd86921ffdc5637aa94e26857d0913c4757
Oleksandr Nemesh <prevter@gmail.com>
2025-01-02 22:39:49 +0200

    implement gd::set for android (#1197)
    * add gnu set headers
    
    * add missing include
    
    * fix stl namespace
    
    * Update stl_iterator_base_types.h

--- loader/include/Geode/c++stl/gnustl/c++config.h
+++ loader/include/Geode/c++stl/gnustl/c++config.h
@@ -8,7 +8,14 @@
 # include "c++config-armeabi-v7a.h"
 #endif
 
+#define _GLIBCXX_NODISCARD __attribute__ ((__warn_unused_result__))
+#define _GLIBCXX20_CONSTEXPR constexpr
+#define _GLIBCXX17_CONSTEXPR constexpr
+#define _GLIBCXX_NOEXCEPT_IF(...) noexcept(__VA_ARGS__)
+
 // i put this here cuz it's used all over the place
 namespace geode::stl {
     template <typename T> using __not_ = std::integral_constant<bool, !T::value>;
+    template <typename... T> using __or_ = std::disjunction<T...>;
+    template <typename... T> using __and_ = std::conjunction<T...>;
 }
