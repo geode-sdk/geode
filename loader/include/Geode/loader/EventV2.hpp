8e3e88234940b1962214bcdb46608f2a024c8ecf
camila314 <47485054+camila314@users.noreply.github.com>
2025-01-13 05:34:31 -0600

    remove geode_dll from event v2 types

--- loader/include/Geode/loader/EventV2.hpp
+++ loader/include/Geode/loader/EventV2.hpp
@@ -4,7 +4,7 @@
 
 namespace geode::event::v2 {
 	template <is_event T>
-	struct GEODE_DLL WrapFilter : public EventFilter<T> {
+	struct WrapFilter : public EventFilter<T> {
 	    std::function<bool(T*)> m_filterFunc;
 
 	    WrapFilter(std::function<bool(T*)> ff) : m_filterFunc(ff) {}
@@ -17,7 +17,7 @@ namespace geode::event::v2 {
 	};
 
 	template <typename T>
-	struct GEODE_DLL EventHandler : public EventListener<WrapFilter<T>> {
+	struct EventHandler : public EventListener<WrapFilter<T>> {
 	    EventHandler(std::function<bool(T*)> filterFunc) : EventListener<WrapFilter<T>>(WrapFilter(filterFunc)) {}
 	};
 }
