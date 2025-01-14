8707a1b857deaf29c608012f9ac25fee305d9698
Cvolton <cvolton@cvolton.eu>
2025-01-14 19:17:06 +0100

    fix SceneManager use after frees

--- loader/src/hooks/persist.cpp
+++ loader/src/hooks/persist.cpp
@@ -1,4 +1,5 @@
 #include <Geode/ui/SceneManager.hpp>
+#include <Geode/modify/CCDirector.hpp>
 
 using namespace geode::prelude;
 
@@ -29,5 +30,28 @@ struct SceneSwitch : Modify<SceneSwitch, AchievementNotifier> {
 };
 
 #endif
+
+struct SceneSwitch2 : Modify<SceneSwitch2, CCDirector> {
+    GEODE_FORWARD_COMPAT_DISABLE_HOOKS("persist disabled")
+    // CCDirector does not call willSwitchToScene in these 2 instances,
+    // so we have to do it ourselves to make everything behave as expected
+    void popScene() {
+        CCDirector::popScene();
+    #ifdef GEODE_IS_WINDOWS
+        AppDelegate::get()->willSwitchToScene(m_pNextScene);
+    #else
+        AchievementNotifier::get()->willSwitchToScene(m_pNextScene);
+    #endif
+    }
+
+    void popToSceneStackLevel(int level) {
+        CCDirector::popToSceneStackLevel(level);
+    #ifdef GEODE_IS_WINDOWS
+        AppDelegate::get()->willSwitchToScene(m_pNextScene);
+    #else
+        AchievementNotifier::get()->willSwitchToScene(m_pNextScene);
+    #endif
+    }
+};
 
 }
