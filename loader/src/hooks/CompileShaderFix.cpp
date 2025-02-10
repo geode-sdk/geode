9d06fa9e487b5171a3c3ee686b170c3321f57f72
Jasmine <52604018+hiimjustin000@users.noreply.github.com>
2025-02-10 10:29:01 -0500

    What if this doesn't work, that would be so hilarious

--- loader/src/hooks/CompileShaderFix.cpp
+++ loader/src/hooks/CompileShaderFix.cpp
@@ -1,8 +1,6 @@
 #include <Geode/Geode.hpp>
 
 // TODO: this affects every platform :P, but i cant be bothered rn
-#ifdef GEODE_IS_WINDOWS
-
 #include <loader/LoaderImpl.hpp>
 
 using namespace geode::prelude;
@@ -15,6 +13,7 @@ $execute {
     // for some reason cocos only properly returns false on winRT, everywhere
     // else it just closes the whole game
 
+#ifdef GEODE_IS_WINDOWS
     auto addr = reinterpret_cast<uintptr_t>(
         GetProcAddress(
             GetModuleHandle("libcocos2d.dll"), "?compileShader@CCGLProgram@cocos2d@@AEAA_NPEAIIPEBD@Z"
@@ -25,9 +24,16 @@ $execute {
         0x31, 0xc0, // xor eax, eax
         0xeb, 0x07 // jmp +7 (to a nearby ret)
     });
+#elif defined(GEODE_IS_ANDROID64)
+    auto addr = reinterpret_cast<uintptr_t>(
+        dlsym(RTLD_DEFAULT, "_ZN7cocos2d11CCGLProgram13compileShaderEPjjPKc")
+    ) + 0xdc;
+
+    (void) Mod::get()->patch(reinterpret_cast<void*>(addr), {
+        0xf5, 0xff, 0xff, 0x17 // b -11 (to a nearby ret)
+    });
+#endif
 #else
     #pragma message("Unsupported GD version!")
 #endif
 };
-
-#endif
