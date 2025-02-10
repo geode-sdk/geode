7be5c4b368c0d75cf03f6633b375935a22c61713
Jasmine <52604018+hiimjustin000@users.noreply.github.com>
2025-02-10 14:45:27 -0500

    macOS...

--- loader/src/hooks/CompileShaderFix.cpp
+++ loader/src/hooks/CompileShaderFix.cpp
@@ -1,6 +1,4 @@
 #include <Geode/Geode.hpp>
-
-// TODO: this affects every platform :P, but i cant be bothered rn
 #include <loader/LoaderImpl.hpp>
 
 using namespace geode::prelude;
@@ -13,7 +11,7 @@ $execute {
     // for some reason cocos only properly returns false on winRT, everywhere
     // else it just closes the whole game
 
-#ifdef GEODE_IS_WINDOWS
+#if defined(GEODE_IS_WINDOWS)
     auto addr = reinterpret_cast<uintptr_t>(
         GetProcAddress(
             GetModuleHandle("libcocos2d.dll"), "?compileShader@CCGLProgram@cocos2d@@AEAA_NPEAIIPEBD@Z"
@@ -39,6 +37,18 @@ $execute {
 
     (void) Mod::get()->patch(reinterpret_cast<void*>(addr), {
         0x14, 0xe0 // b +2c (skip if statement)
+    });
+#elif defined(GEODE_IS_ARM_MAC)
+    auto addr = base::get() + 0x393aa0;
+
+    (void) Mod::get()->patch(reinterpret_cast<void*>(addr), {
+        0x1f, 0x20, 0x03, 0xd5 // nop (skip if statement)
+    });
+#elif defined(GEODE_IS_INTEL_MAC)
+    auto addr = base::get() + 0x417f65;
+
+    (void) Mod::get()->patch(reinterpret_cast<void*>(addr), {
+        0x48, 0x90, // nop (skip if statement)
     });
 #endif
 #else
