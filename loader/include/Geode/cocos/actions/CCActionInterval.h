d96a67c956764a294f53e2bb9f45e1d64c245b75
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2025-02-19 20:08:43 -0500

    Whoops

--- loader/include/Geode/cocos/actions/CCActionInterval.h
+++ loader/include/Geode/cocos/actions/CCActionInterval.h
@@ -233,7 +233,7 @@ class CC_DLL CCRepeatForever : public CCActionInterval
 {
     GEODE_FRIEND_MODIFY
 public:
-    GEODE_CUSTOM_CONSTRUCTOR_COCOS(CCRepeatForerver, CCActionInterval);
+    GEODE_CUSTOM_CONSTRUCTOR_COCOS(CCRepeatForever, CCActionInterval)
     /**
      *  @js ctor
      */
