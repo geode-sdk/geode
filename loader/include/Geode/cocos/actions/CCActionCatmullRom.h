37b97cc72924238389f474cfb7e1936c8d0c3d3b
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2025-03-11 19:45:45 -0400

    revert deletions

--- loader/include/Geode/cocos/actions/CCActionCatmullRom.h
+++ loader/include/Geode/cocos/actions/CCActionCatmullRom.h
@@ -59,6 +59,7 @@ class CC_DLL CCPointArray : public CCObject
 {
     GEODE_FRIEND_MODIFY
 public:
+
     GEODE_CUSTOM_CONSTRUCTOR_COCOS(CCPointArray, CCObject)
 
     /** creates and initializes a Points array with capacity 
@@ -231,6 +232,7 @@ class CC_DLL CCCatmullRomTo : public CCCardinalSplineTo
 {
     GEODE_FRIEND_MODIFY
 public:
+
     /** creates an action with a Cardinal Spline array of points and tension 
      *  @code
      *  when this function bound to js,the input params are changed
@@ -255,6 +257,7 @@ class CC_DLL CCCatmullRomBy : public CCCardinalSplineBy
 {
     GEODE_FRIEND_MODIFY
 public:
+
     /** creates an action with a Cardinal Spline array of points and tension 
      *  @code
      *  when this function bound to js,the input params are changed
