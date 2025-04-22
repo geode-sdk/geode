07857a8116dee715545eca7b06e861d71d879c53
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2025-04-22 14:02:26 -0400

    Back to C++

--- loader/src/hooks/ShutdownFix.cpp
+++ loader/src/hooks/ShutdownFix.cpp
@@ -2,7 +2,7 @@
 #ifdef GEODE_IS_MACOS
 #include <Geode/loader/Mod.hpp>
 #include <Geode/utils/ObjcHook.hpp>
-#include <objc/NSObject.h>
+#include <objc/message.h>
 
 using namespace geode::prelude;
 
@@ -10,12 +10,12 @@ using namespace geode::prelude;
 // where the game tries to call Steam API functions
 // after the Steam API has been shut down.
 
-void shutdownGameHook(NSObject* self, SEL sel) {
+void shutdownGameHook(void* self, SEL sel) {
     auto director = CCDirector::sharedDirector();
     director->pause();
     director->getScheduler()->unscheduleAll();
     // call the original
-    [self performSelector:sel];
+    reinterpret_cast<void (*)(void*, SEL)>(objc_msgSend)(self, sel);
 }
 
 $execute {
