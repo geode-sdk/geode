e1f5983dcaf98b96b929e9585ece0cda18218397
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2025-04-30 15:22:17 +0200

    add lazysprite (todo docs)

---
+++ loader/include/Geode/ui/LazySprite.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "LoadingSpinner.hpp"
+#include <Geode/utils/web.hpp>
+
+#include <cocos2d.h>
+#include <filesystem>
+#include <stdint.h>
+
+namespace geode {
+    /**
+     * A sprite that is initialized that is loaded asynchronously.
+     * It can be initialized to either a blank texture or a loading circle,
+     * and then given either a URL, file path, or raw image data that will be processed
+     * in the background to avoid freezes.
+     */
+    class GEODE_DLL LazySprite : public cocos2d::CCSprite {
+    public:
+        using Callback = std::function<void(Result<>)>;
+        using Format = cocos2d::CCImage::EImageFormat;
+
+        static LazySprite* create(cocos2d::CCSize size, bool loadingCircle = true);
+
+        void loadFromUrl(std::string const& url, Format format = Format::kFmtUnKnown, bool ignoreCache = false);
+        void loadFromUrl(char const* url, Format format = Format::kFmtUnKnown, bool ignoreCache = false);
+        void loadFromFile(std::filesystem::path const& path, Format format = Format::kFmtUnKnown, bool ignoreCache = false);
+        void loadFromData(std::span<uint8_t const> data, Format format = Format::kFmtUnKnown);
+        void loadFromData(uint8_t const* ptr, size_t size, Format format = Format::kFmtUnKnown);
+        void loadFromData(std::vector<uint8_t> data, Format format = Format::kFmtUnKnown);
+
+        /**
+         * Set the callback to be called once the sprite is fully loaded, or an error occurred.
+         * @param callback The callback
+         */
+        void setLoadCallback(Callback callback);
+
+        /**
+         * Set whether the image will be automatically resized to the size given in the `create` / `init` function
+         * once it's loaded. By default is `false`.
+         */
+        void setAutoResize(bool value);
+
+        /**
+         * Returns whether the image is now loaded
+         */
+        bool isLoaded();
+        bool isLoading();
+
+        virtual bool initWithTexture(cocos2d::CCTexture2D* texture) override;
+        virtual bool initWithTexture(cocos2d::CCTexture2D* pTexture, const cocos2d::CCRect& rect) override;
+        virtual bool initWithSpriteFrame(cocos2d::CCSpriteFrame* pSpriteFrame) override;
+        virtual bool initWithSpriteFrameName(const char* pszSpriteFrameName) override;
+        virtual bool initWithFile(const char* pszFilename) override;
+        virtual bool initWithFile(const char* pszFilename, const cocos2d::CCRect& rect) override;
+
+    protected:
+        Ref<LoadingSpinner> m_loadingCircle;
+        Callback m_callback;
+        Format m_expectedFormat;
+        EventListener<utils::web::WebTask> m_listener;
+        bool m_isLoading = false;
+        bool m_hasLoaded = false;
+        cocos2d::CCSize m_targetSize;
+        bool m_autoresize;
+
+        bool init(cocos2d::CCSize size, bool loadingCircle = true);
+        void doInitFromBytes(std::vector<uint8_t> data, std::string cacheKey);
+        std::string makeCacheKey(std::filesystem::path const& path);
+        // std::string makeCacheKey(std::string_view url);
+
+        cocos2d::CCTexture2D* lookupCache(char const* key);
+        bool initFromCache(char const* key);
+        bool postInit(bool initResult);
+
+        void onError(std::string err);
+    };
+}
