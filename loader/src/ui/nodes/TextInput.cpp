803bf40b0b1626999c4e06bf3fd4b428b53ac36f
JohnnyCena123 <iamahuman1395@gmail.com>
2025-05-02 20:14:12 +0300

    all tabs are now spaces again :3

--- loader/src/ui/nodes/TextInput.cpp
+++ loader/src/ui/nodes/TextInput.cpp
@@ -7,15 +7,15 @@
 using namespace geode::prelude;
 
 struct TextInputNodeFix : Modify<TextInputNodeFix, CCTextInputNode> {
-	GEODE_FORWARD_COMPAT_DISABLE_HOOKS("TextInputNode fix")
+    GEODE_FORWARD_COMPAT_DISABLE_HOOKS("TextInputNode fix")
 
-	bool init(float p0, float p1, char const* p2, char const* p3, int p4, char const* p5) {
-		if (!CCTextInputNode::init(p0, p1, p2, p3, p4, p5)) return false;
-		m_placeholderLabel->limitLabelWidth(m_obContentSize.width - 10.f, .5f, .1f);
-		return true;
-	}
+    bool init(float p0, float p1, char const* p2, char const* p3, int p4, char const* p5) {
+        if (!CCTextInputNode::init(p0, p1, p2, p3, p4, p5)) return false;
+        m_placeholderLabel->limitLabelWidth(m_obContentSize.width - 10.f, .5f, .1f);
+        return true;
+    }
 
-	bool ccTouchBegan(CCTouch* touch, CCEvent* event) {
+    bool ccTouchBegan(CCTouch* touch, CCEvent* event) {
         if (!this->getUserObject("fix-text-input")) {
             return CCTextInputNode::ccTouchBegan(touch, event);
         }
@@ -57,171 +57,171 @@ struct TextInputNodeFix : Modify<TextInputNodeFix, CCTextInputNode> {
 };
 
 const char* geode::getCommonFilterAllowedChars(CommonFilter filter) {
-	switch (filter) {
-		default:
-		case CommonFilter::Uint:         return "0123456789";
-		case CommonFilter::Int:          return "-0123456789";
-		case CommonFilter::Float:        return "-.0123456789";
-		case CommonFilter::ID:           return "abcdefghijklmnopqrstuvwxyz0123456789-_.";
-		case CommonFilter::Name:         return "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_ ";
-		case CommonFilter::Any:          return "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ#_-+/\\&$%^~*\'\"{}()[]<>=!?@,;.:|• ";
-		case CommonFilter::Hex:          return "0123456789abcdefABCDEF";
-		case CommonFilter::Base64Normal: return "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/=";
-		case CommonFilter::Base64URL:    return "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_=";
-		case CommonFilter::Alphanumeric: return "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    switch (filter) {
+        default:
+        case CommonFilter::Uint:         return "0123456789";
+        case CommonFilter::Int:          return "-0123456789";
+        case CommonFilter::Float:        return "-.0123456789";
+        case CommonFilter::ID:           return "abcdefghijklmnopqrstuvwxyz0123456789-_.";
+        case CommonFilter::Name:         return "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_ ";
+        case CommonFilter::Any:          return "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ#_-+/\\&$%^~*\'\"{}()[]<>=!?@,;.:|• ";
+        case CommonFilter::Hex:          return "0123456789abcdefABCDEF";
+        case CommonFilter::Base64Normal: return "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/=";
+        case CommonFilter::Base64URL:    return "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_=";
+        case CommonFilter::Alphanumeric: return "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         case CommonFilter::Alphabetic:   return "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	}
+    }
 }
 
 bool TextInput::init(float width, std::string const& placeholder, std::string const& font) {
-	if (!CCNode::init())
-		return false;
+    if (!CCNode::init())
+        return false;
 
-	constexpr float HEIGHT = 30.f;
+    constexpr float HEIGHT = 30.f;
 
-	this->setContentSize({ width, HEIGHT });
-	this->setAnchorPoint({ .5f, .5f });
+    this->setContentSize({ width, HEIGHT });
+    this->setAnchorPoint({ .5f, .5f });
 
-	m_bgSprite = extension::CCScale9Sprite::create("square02b_001.png", { 0, 0, 80, 80 });
-	m_bgSprite->setScale(.5f);
-	m_bgSprite->setColor({ 0, 0, 0 });
-	m_bgSprite->setOpacity(90);
-	m_bgSprite->setContentSize({ width * 2, HEIGHT * 2 });
-	this->addChildAtPosition(m_bgSprite, Anchor::Center);
+    m_bgSprite = extension::CCScale9Sprite::create("square02b_001.png", { 0, 0, 80, 80 });
+    m_bgSprite->setScale(.5f);
+    m_bgSprite->setColor({ 0, 0, 0 });
+    m_bgSprite->setOpacity(90);
+    m_bgSprite->setContentSize({ width * 2, HEIGHT * 2 });
+    this->addChildAtPosition(m_bgSprite, Anchor::Center);
 
-	m_input = CCTextInputNode::create(width - 10.f, HEIGHT, placeholder.c_str(), 24, font.c_str());
-	m_input->setLabelPlaceholderColor({ 150, 150, 150 });
-	m_input->setLabelPlaceholderScale(.5f);
-	m_input->setMaxLabelScale(.6f);
-	m_input->setUserObject("fix-text-input", CCBool::create(true));
-	this->addChildAtPosition(m_input, Anchor::Center);
+    m_input = CCTextInputNode::create(width - 10.f, HEIGHT, placeholder.c_str(), 24, font.c_str());
+    m_input->setLabelPlaceholderColor({ 150, 150, 150 });
+    m_input->setLabelPlaceholderScale(.5f);
+    m_input->setMaxLabelScale(.6f);
+    m_input->setUserObject("fix-text-input", CCBool::create(true));
+    this->addChildAtPosition(m_input, Anchor::Center);
 
-	return true;
+    return true;
 }
 
 TextInput* TextInput::create(float width, std::string const& placeholder, std::string const& font) {
-	auto ret = new TextInput();
-	if (ret->init(width, placeholder, font)) {
-		ret->autorelease();
-		return ret;
-	}
-	delete ret;
-	return nullptr;
+    auto ret = new TextInput();
+    if (ret->init(width, placeholder, font)) {
+        ret->autorelease();
+        return ret;
+    }
+    delete ret;
+    return nullptr;
 }
 
 void TextInput::textChanged(CCTextInputNode* input) {
-	if (m_onInput) {
-		m_onInput(input->getString());
-	}
+    if (m_onInput) {
+        m_onInput(input->getString());
+    }
 }
 
 void TextInput::setPlaceholder(std::string const& placeholder) {
-	m_input->m_caption = placeholder;
-	m_input->refreshLabel();
+    m_input->m_caption = placeholder;
+    m_input->refreshLabel();
 }
 void TextInput::setLabel(std::string const& label) {
-	if (label.size()) {
-		if (m_label) {
-			m_label->setString(label.c_str());
-		}
-		else {
-			m_label = CCLabelBMFont::create(label.c_str(), "goldFont.fnt");
-			this->addChildAtPosition(m_label, Anchor::TopLeft, ccp(3, 2), ccp(0, 0));
-		}
-		m_label->limitLabelWidth(m_bgSprite->getScaledContentWidth() - 6, .4f, .1f);
-	}
-	else {
-		if (m_label) {
-			m_label->removeFromParent();
-			m_label = nullptr;
-		}
-	}
+    if (label.size()) {
+        if (m_label) {
+            m_label->setString(label.c_str());
+        }
+        else {
+            m_label = CCLabelBMFont::create(label.c_str(), "goldFont.fnt");
+            this->addChildAtPosition(m_label, Anchor::TopLeft, ccp(3, 2), ccp(0, 0));
+        }
+        m_label->limitLabelWidth(m_bgSprite->getScaledContentWidth() - 6, .4f, .1f);
+    }
+    else {
+        if (m_label) {
+            m_label->removeFromParent();
+            m_label = nullptr;
+        }
+    }
 }
 void TextInput::setFilter(std::string const& allowedChars) {
-	m_input->m_allowedChars = allowedChars;
+    m_input->m_allowedChars = allowedChars;
 }
 void TextInput::setCommonFilter(CommonFilter filter) {
-	this->setFilter(getCommonFilterAllowedChars(filter));
+    this->setFilter(getCommonFilterAllowedChars(filter));
 }
 void TextInput::setMaxCharCount(size_t length) {
-	m_input->m_maxLabelLength = length == 0 ? 9999999 : length;
+    m_input->m_maxLabelLength = length == 0 ? 9999999 : length;
 }
 void TextInput::setPasswordMode(bool enable) {
-	m_input->m_usePasswordChar = enable;
-	m_input->refreshLabel();
+    m_input->m_usePasswordChar = enable;
+    m_input->refreshLabel();
 }
 void TextInput::setWidth(float width) {
-	this->setContentWidth(width);
-	m_input->m_maxLabelWidth = width - 10.f;
-	m_input->setContentWidth(width);
-	m_bgSprite->setContentWidth(width * 2);
-	m_input->setPositionX(width / 2.f);
-	m_bgSprite->setPositionX(width / 2.f);
+    this->setContentWidth(width);
+    m_input->m_maxLabelWidth = width - 10.f;
+    m_input->setContentWidth(width);
+    m_bgSprite->setContentWidth(width * 2);
+    m_input->setPositionX(width / 2.f);
+    m_bgSprite->setPositionX(width / 2.f);
 }
 void TextInput::setDelegate(TextInputDelegate* delegate, std::optional<int> tag) {
-	m_input->m_delegate = delegate;
-	m_onInput = nullptr;
-	if (tag.has_value()) {
-		m_input->setTag(tag.value());
-	}
+    m_input->m_delegate = delegate;
+    m_onInput = nullptr;
+    if (tag.has_value()) {
+        m_input->setTag(tag.value());
+    }
 }
 void TextInput::setCallback(std::function<void(std::string const&)> onInput) {
-	this->setDelegate(this);
-	m_onInput = onInput;
+    this->setDelegate(this);
+    m_onInput = onInput;
 }
 void TextInput::setEnabled(bool enabled) {
-	m_input->setTouchEnabled(enabled);
-	m_input->m_placeholderLabel->setOpacity(enabled ? 255 : 150);
+    m_input->setTouchEnabled(enabled);
+    m_input->m_placeholderLabel->setOpacity(enabled ? 255 : 150);
 }
 void TextInput::setTextAlign(TextInputAlign align) {
-	switch (align) {
-		default:
-		case TextInputAlign::Center: {
-			m_input->m_textField->setAnchorPoint({ .5f, .5f });
-			m_input->m_placeholderLabel->setAnchorPoint({ .5f, .5f });
-			m_input->updateAnchoredPosition(Anchor::Center);
-		} break;
+    switch (align) {
+        default:
+        case TextInputAlign::Center: {
+            m_input->m_textField->setAnchorPoint({ .5f, .5f });
+            m_input->m_placeholderLabel->setAnchorPoint({ .5f, .5f });
+            m_input->updateAnchoredPosition(Anchor::Center);
+        } break;
 
-		case TextInputAlign::Left: {
-			m_input->m_textField->setAnchorPoint({ .0f, .5f });
-			m_input->m_placeholderLabel->setAnchorPoint({ .0f, .5f });
-			m_input->updateAnchoredPosition(Anchor::Left, ccp(5, 0));
-		} break;
-	}
+        case TextInputAlign::Left: {
+            m_input->m_textField->setAnchorPoint({ .0f, .5f });
+            m_input->m_placeholderLabel->setAnchorPoint({ .0f, .5f });
+            m_input->updateAnchoredPosition(Anchor::Left, ccp(5, 0));
+        } break;
+    }
 }
 
 void TextInput::hideBG() {
-	m_bgSprite->setVisible(false);
+    m_bgSprite->setVisible(false);
 }
 
 void TextInput::setString(std::string const& str, bool triggerCallback) {
-	auto oldDelegate = m_input->m_delegate;
-	// Avoid triggering the callback
-	m_input->m_delegate = nullptr;
-	m_input->setString(str);
-	m_input->m_delegate = oldDelegate;
-	if (triggerCallback && m_input->m_delegate) {
-		m_input->m_delegate->textChanged(m_input);
-	}
+    auto oldDelegate = m_input->m_delegate;
+    // Avoid triggering the callback
+    m_input->m_delegate = nullptr;
+    m_input->setString(str);
+    m_input->m_delegate = oldDelegate;
+    if (triggerCallback && m_input->m_delegate) {
+        m_input->m_delegate->textChanged(m_input);
+    }
 }
 
 std::string TextInput::getString() const {
-	return m_input->getString();
+    return m_input->getString();
 }
 
 void TextInput::focus() {
-	m_input->onClickTrackNode(true);
+    m_input->onClickTrackNode(true);
 }
 void TextInput::defocus() {
-	m_input->detachWithIME();
+    m_input->detachWithIME();
 }
 
 CCTextInputNode* TextInput::getInputNode() const {
-	return m_input;
+    return m_input;
 }
 CCScale9Sprite* TextInput::getBGSprite() const {
-	return m_bgSprite;
+    return m_bgSprite;
 }
 CCLabelBMFont* TextInput::getLabel() const {
-	return m_label;
+    return m_label;
 }
