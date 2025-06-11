2a4566c53f5404e48ee2df70597a145043228db8
stetupdev <jacobstetup@tuta.io>
2025-06-11 15:08:28 -0400

    Add Linux Support (I Hope)

--- cmake/PlatformDetect.cmake
+++ cmake/PlatformDetect.cmake
@@ -1,19 +1,50 @@
-if (NOT DEFINED GEODE_TARGET_PLATFORM)
-	if(APPLE)
-		if("${CMAKE_SYSTEM_NAME}" STREQUAL "iOS" OR IOS)
-			set(GEODE_TARGET_PLATFORM "iOS")
-		else()
-			set(GEODE_TARGET_PLATFORM "MacOS")
-		endif()
-	elseif(WIN32)
-		set(GEODE_TARGET_PLATFORM "Win64")
-	elseif(ANDROID)
-		if (ANDROID_ABI STREQUAL "arm64-v8a")
-			set(GEODE_TARGET_PLATFORM "Android64")
-		elseif(ANDROID_ABI STREQUAL "armeabi-v7a")
-			set(GEODE_TARGET_PLATFORM "Android32")
-		endif()
-	else()
-		message(FATAL_ERROR "Unable to detect platform, please set GEODE_TARGET_PLATFORM in the root CMake file.")
-	endif()
+# PlatformDetect.cmake
+
+# Clear previous platform variable
+unset(GEODE_TARGET_PLATFORM CACHE)
+
+if(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+    # Detect MacOS vs iOS
+    if(IOS)
+        set(GEODE_TARGET_PLATFORM "iOS")
+    else()
+        # Use CMAKE_OSX_SYSROOT to distinguish iOS SDK from MacOS
+        if(CMAKE_OSX_SYSROOT MATCHES "iphoneos")
+            set(GEODE_TARGET_PLATFORM "iOS")
+        else()
+            set(GEODE_TARGET_PLATFORM "MacOS")
+        endif()
+    endif()
+
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+    if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+        set(GEODE_TARGET_PLATFORM "Win64")
+    else()
+        message(FATAL_ERROR "32-bit Windows is not supported")
+    endif()
+
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+    # Support both 32-bit and 64-bit, but default to Linux platform
+    if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+        set(GEODE_TARGET_PLATFORM "Linux")
+    elseif(CMAKE_SIZEOF_VOID_P EQUAL 4)
+        set(GEODE_TARGET_PLATFORM "Linux")
+    else()
+        message(FATAL_ERROR "Unknown Linux architecture pointer size: ${CMAKE_SIZEOF_VOID_P}")
+    endif()
+
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Android")
+    # Android can be 32-bit or 64-bit
+    if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+        set(GEODE_TARGET_PLATFORM "Android64")
+    elseif(CMAKE_SIZEOF_VOID_P EQUAL 4)
+        set(GEODE_TARGET_PLATFORM "Android32")
+    else()
+        message(FATAL_ERROR "Unknown Android architecture pointer size: ${CMAKE_SIZEOF_VOID_P}")
+    endif()
+
+else()
+    message(FATAL_ERROR "Unsupported platform: ${CMAKE_SYSTEM_NAME}")
 endif()
+
+message(STATUS "Detected platform: ${GEODE_TARGET_PLATFORM}")
