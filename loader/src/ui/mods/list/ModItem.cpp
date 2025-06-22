0b0801301da70a63dc89051f736d6be164d6e88c
alk <45172705+altalk23@users.noreply.github.com>
2025-06-22 18:55:42 +0300

    Update ModItem.cpp

--- loader/src/ui/mods/list/ModItem.cpp
+++ loader/src/ui/mods/list/ModItem.cpp
@@ -264,7 +264,7 @@ bool ModItem::init(ModSource&& source) {
             if (metadata.tags.contains("joke")) {
                 m_badgeContainer->addChild(CCSprite::createWithSpriteFrameName("tag-joke.png"_spr));
             }
-            if (metadata.tags.contains("api")) {
+            if (metadata.tags.contains("api") || metadata.isAPI()) {
                 m_badgeContainer->addChild(CCSprite::createWithSpriteFrameName("tag-api.png"_spr));
             }
             if (metadata.tags.contains("modtober24winner") || m_source.getID() == "rainixgd.geome3dash") {
