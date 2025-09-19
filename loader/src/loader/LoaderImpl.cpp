c1999e43d372dedd4ad5721d66f51189f95062ed
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2025-09-19 16:39:46 -0400

    You know what?

--- loader/src/loader/LoaderImpl.cpp
+++ loader/src/loader/LoaderImpl.cpp
@@ -787,9 +787,8 @@ void Loader::Impl::orderModStack() {
         }
     }
 
-    m_modsToLoad.insert(m_modsToLoad.end(), dependants.begin(), dependants.end());
-
-    for (auto mod : m_modsToLoad) {
+    for (auto mod : dependants) {
+        m_modsToLoad.push_back(mod);
         log::debug("{}, early: {}", mod->getID(), mod->needsEarlyLoad());
     }
 }
