2a1630a82f226c37b4481b98c6d300486e1c0c5c
SMJS <38814077+SMJSGaming@users.noreply.github.com>
2025-09-30 19:50:34 +0200

    Added Modtober 2025 content (#1501)
    * Added Modtober 2025 content
    
    * Fixed a copy paste error in the popup image and replaced the weird string manipulation with fmt::format

--- loader/src/ui/mods/list/ModList.hpp
+++ loader/src/ui/mods/list/ModList.hpp
@@ -71,7 +71,7 @@ protected:
     void onToggleUpdates(CCObject*);
     void onToggleErrors(CCObject*);
     void onUpdateAll(CCObject*);
-    void onEventInfo(CCObject*);
+    void onModtoberInfo(CCObject*);
 
 public:
     static ModList* create(ModListSource* src, CCSize const& size);
