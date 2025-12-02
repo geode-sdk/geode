b79c044f0c8387b3268fe69cac59381d063e19a4
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2025-12-02 23:17:51 +0100

    random fixes

--- loader/include/Geode/utils/random.hpp
+++ loader/include/Geode/utils/random.hpp
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <type_traits>
 #include <cstring>
+#include <array>
 
 namespace geode::utils::random {
     GEODE_DLL std::array<uint64_t, 3>& _getState();
@@ -52,10 +53,12 @@ namespace geode::utils::random {
     /// meaning the max value can never be returned
     template <typename T, typename Y, typename Out = std::common_type_t<T, Y>>
     Out generate(T min, Y max) {
+        if (max <= min) return min;
+
         if constexpr (std::is_integral_v<Out>) {
-            uint64_t range = static_cast<uint64_t>(max) - static_cast<uint64_t>(min);
+            Out range = max - min;
             uint64_t val = nextU64();
-            return static_cast<Out>(static_cast<uint64_t>(min) + (val % range));
+            return static_cast<Out>(min) + (val % range);
         } else if constexpr (std::is_floating_point_v<Out>) {
             double range = static_cast<double>(max) - static_cast<double>(min);
             double val = generate<double>();
@@ -66,24 +69,21 @@ namespace geode::utils::random {
     }
 
     /// Shuffles an array of elements in place.
-    /// Use shuffle(std::vector<T>&) or shuffle(std::array<T, N>&) for easier usage.
-    template <typename T>
-    void shuffle(T* data, size_t count) {
-        for (ptrdiff_t i = count - 1; i > 0; i--) {
+    /// Use the shuffle(T&) overload for easier usage if your container supports iterators
+    template <typename Iter>
+    void shuffle(Iter begin, Iter end) {
+        using diff_t = std::iterator_traits<Iter>::difference_type;
+        for (diff_t i = end - begin - 1; i > 0; i--) {
             using std::swap;
-            auto j = generate<ptrdiff_t>(0, i + 1);
-            swap(data[i], data[j]);
+            auto j = generate<diff_t>(0, i + 1);
+            swap(*(begin + i), *(begin + j));
         }
     }
 
-    template <typename T>
-    void shuffle(std::vector<T>& vec) {
-        shuffle(vec.data(), vec.size());
-    }
-
-    template <typename T, size_t N>
-    void shuffle(std::array<T, N>& arr) {
-        shuffle(arr.data(), arr.size());
+    template <typename Cont>
+    void shuffle(Cont& vec) {
+        using std::begin, std::end;
+        shuffle(begin(vec), end(vec));
     }
 
     GEODE_DLL std::string generateString(size_t length, std::string_view alphabet);
