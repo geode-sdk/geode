efdbc58cd0c64f64fa0a7ccac754e6860c508434
altalk23 <45172705+altalk23@users.noreply.github.com>
2025-12-19 14:41:07 +0300

    uh

--- loader/src/loader/Dispatch.cpp
+++ loader/src/loader/Dispatch.cpp
@@ -2,16 +2,10 @@
 
 using namespace geode::prelude;
 
-<<<<<<< HEAD
 EventListenerPool* geode::getDispatchPool(std::string const& id) {
     static std::unordered_map<std::string, std::shared_ptr<DefaultEventListenerPool>> pools;
     if (pools.count(id) == 0) {
         pools[id] = DefaultEventListenerPool::create();
     }
     return pools[id].get();
-=======
-StringMap<EventListenerPool*>& geode::dispatchPools() {
-    static StringMap<EventListenerPool*> pools;
-    return pools;
->>>>>>> v5
 }
