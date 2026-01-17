4b1ead3f15ea45174e954ce6b2bcff96ce10a2e2
PoweredByPie <poweredbypie@users.noreply.github.com>
2026-01-16 18:46:01 -0800

    use full cmdlet flags for readability + dedup name

--- installer/windows/dl-vcr.ps1
+++ installer/windows/dl-vcr.ps1
@@ -1,6 +1,7 @@
 # Downloads the Visual Studio runtime that is necessary for building the Windows installer.
-if (Test-Path VC_redist.x64.exe -PathType leaf) {
+$Out = 'VC_redist.x64.exe'
+if (Test-Path $Out -PathType Leaf) {
     Return
 }
 $ProgressPreference = 'SilentlyContinue'
-Invoke-WebRequest -useb https://aka.ms/vs/17/release/vc_redist.x64.exe -outfile VC_redist.x64.exe
+Invoke-WebRequest -UseBasicParsing 'https://aka.ms/vs/17/release/vc_redist.x64.exe' -OutFile $Out
