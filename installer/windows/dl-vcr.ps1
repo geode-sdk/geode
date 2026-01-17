bf9d77a8d943b08d33ebdd8e34a5cd8548265a89
PoweredByPie <poweredbypie@users.noreply.github.com>
2026-01-16 18:42:07 -0800

    make vc runtime downloader significantly faster
    this disables powershell's progress bar when downloading which slows
    down Invoke-WebRequest to a crawl.

--- installer/windows/dl-vcr.ps1
+++ installer/windows/dl-vcr.ps1
@@ -1,4 +1,6 @@
-if(Test-path VC_redist.x64.exe -PathType leaf) {
+# Downloads the Visual Studio runtime that is necessary for building the Windows installer.
+if (Test-Path VC_redist.x64.exe -PathType leaf) {
     Return
 }
+$ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -useb https://aka.ms/vs/17/release/vc_redist.x64.exe -outfile VC_redist.x64.exe
