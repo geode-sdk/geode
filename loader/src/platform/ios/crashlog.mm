a70c6337b52ea68a2e49ad43bd487b56786d8003
prevter <prevter@gmail.com>
2026-01-29 15:31:05 +0200

    modernize ios crashlog

--- loader/src/platform/ios/crashlog.mm
+++ loader/src/platform/ios/crashlog.mm
@@ -189,69 +189,75 @@ static std::string formatAddress(void const* addr) {
 }
 
 static std::string getInfo(void* address, Mod* faultyMod) {
-    std::stringstream stream;
+    StringBuffer<> stream;
     auto image = imageFromAddress(address);
     auto imageName = getImageName(image);
-    stream << "Faulty Lib: " << imageName << "\n";
-    stream << "Faulty Mod: " << (faultyMod ? faultyMod->getID() : "<Unknown>") << "\n";
-    stream << "Instruction Address: " << formatAddress(address) << "\n";
-    stream << fmt::format("Signal Code: 0x{:x} ({})\n", s_signal, getSignalCodeString());
+    stream.append(
+        "Faulty Lib: {}\n"
+        "Faulty Mod: {}\n"
+        "Instruction Address: {}\n"
+        "Signal Code: 0x{:x} ({})\n",
+        imageName,
+        (faultyMod ? faultyMod->getID() : "<Unknown>"),
+        formatAddress(address),
+        s_signal, getSignalCodeString()
+    );
 
     // these 5 have the si_addr field available as per `man sigaction`
     if (s_signal == SIGILL || s_signal == SIGFPE || s_signal == SIGSEGV || s_signal == SIGBUS || s_signal == SIGTRAP) {
-        stream << "Signal Detail: ";
+        stream.append("Signal Detail: ");
 
         switch (s_signal) {
             case SIGILL: {
-                stream << fmt::format("Illegal instruction was encountered at {}", formatAddress(s_siginfo->si_addr));
+                stream.append("Illegal instruction was encountered at {}", formatAddress(s_siginfo->si_addr));
             } break;
 
             case SIGFPE: {
-                stream << fmt::format("Floating point exception was thrown at {}", formatAddress(s_siginfo->si_addr));
+                stream.append("Floating point exception was thrown at {}", formatAddress(s_siginfo->si_addr));
             } break;
 
             case SIGSEGV: {
-                stream << fmt::format("Could not access memory at {} (", formatAddress(s_siginfo->si_addr));
+                stream.append("Could not access memory at {} (", formatAddress(s_siginfo->si_addr));
 
                 switch (s_siginfo->si_code) {
                     case SEGV_MAPERR: {
-                        stream << "address not mapped to an object";
+                        stream.append("address not mapped to an object");
                     } break;
 
                     case SEGV_ACCERR: {
-                        stream << "invalid permissions for mapped object";
+                        stream.append("invalid permissions for mapped object");
                     } break;
                 }
 
-                stream << ")";
+                stream.append(')');
             } break;
 
             case SIGBUS: {
-                stream << fmt::format("Bus error when trying to access memory at {} (", formatAddress(s_siginfo->si_addr));
+                stream.append("Bus error when trying to access memory at {} (", formatAddress(s_siginfo->si_addr));
 
                 switch (s_siginfo->si_code) {
                     case BUS_ADRALN: {
-                        stream << "invalid address alignment";
+                        stream.append("invalid address alignment");
                     } break;
 
                     case BUS_ADRERR: {
-                        stream << "nonexistent physical address";
+                        stream.append("nonexistent physical address");
                     } break;
 
                     case BUS_OBJERR: {
-                        stream << "object-specific hardware error";
+                        stream.append("object-specific hardware error");
                     } break;
                 }
 
-                stream << ")";
+                stream.append(')');
             } break;
 
             case SIGTRAP: {
-                stream << fmt::format("Breakpoint was hit at {}", formatAddress(s_siginfo->si_addr));
+                stream.append("Breakpoint was hit at {}", formatAddress(s_siginfo->si_addr));
             } break;
         }
 
-        stream << "\n";
+        stream.append('\n');
     }
 
     return stream.str();
@@ -276,12 +282,12 @@ extern "C" void signalHandler(int signal, siginfo_t* signalInfo, void* vcontext)
 
 // https://stackoverflow.com/questions/8278691/how-to-fix-backtrace-line-number-error-in-c
 std::string executeCommand(ZStringView cmd) {
-    std::stringstream stream;
+    StringBuffer<> stream;
     std::array<char, 1024> buf;
 
     if (FILE* ptr = popen(cmd.c_str(), "r")) {
         while (fgets( buf.data(), buf.size(), ptr ) != NULL) {
-            stream << buf.data();
+            stream.append("{}", buf.data());
         }
         pclose(ptr);
     }
@@ -290,17 +296,17 @@ std::string executeCommand(ZStringView cmd) {
 }
 
 std::string addr2Line() {
-    std::stringstream stream;
-    stream << "atos -p " << getpid() << " ";
+    StringBuffer<> stream;
+    stream.append("atos -p {} ", getpid());
     for (int i = 1; i < s_backtraceSize; ++i) {
-        stream << s_backtrace[i] << " ";
+        stream.append("{} ", s_backtrace[i]);
     }
     // std::cout << "command: " << stream.str() << std::endl;
     return executeCommand(stream.str());
 }
 
 static std::string getStacktrace() {
-    std::stringstream stacktrace;
+    StringBuffer<> stacktrace;
 
     auto messages = backtrace_symbols(s_backtrace.data(), s_backtraceSize);
     if (s_backtraceSize < FRAME_SIZE) {
@@ -342,18 +348,17 @@ static std::string getStacktrace() {
             // log::debug("address: {}", address);
             auto image = imageFromAddress(reinterpret_cast<void*>(address));
             // log::debug("image: {}", image);
-            stacktrace << " - " << std::showbase << std::hex;
+            stacktrace.append(" - ");
 
             if (image) {
                 auto baseAddress = image->imageLoadAddress;
                 auto imageName = getImageName(image);
-                stacktrace << imageName << " + " << (address - (uintptr_t)baseAddress);
+                stacktrace.append("{} + 0x{:x}", imageName, address - (uintptr_t)baseAddress);
             }
             else {
-                stacktrace << address;
+                stacktrace.append("0x{:x}", address);
             }
-            stacktrace << std::dec;
-            stacktrace << ": " << line << "\n";
+            stacktrace.append(": {}\n", line);
         }
         else {
             if (i > 0) {
@@ -377,22 +382,24 @@ static std::string getStacktrace() {
             // don't display the (function + offset) part if it will be bogus.
             // the first case (0x0) happens with hook handlers, while the second happens because
             // GD exports a few fmt symbols, so backtrace_symbols thinks every function in GD is the last fmt symbol in the binary
-            if (function == "0x0" || (binary == "GeometryJump" && offset > 0x1000)) {
+            if (function == "0x0") {
                 function = "";
+            } else if (binary == "GeometryJump" && offset > 0x1000) {
+                function = crashlog::lookupClosestFunction(offset);
             }
 
             if (auto image = imageFromAddress(reinterpret_cast<void*>(address))) {
-                stacktrace << "- " << getImageName(image) << " + " << std::showbase << std::hex << (address - (uintptr_t)image->imageLoadAddress) << std::dec;
+                stacktrace.append("- {} + 0x{:x}", getImageName(image), address - (uintptr_t)image->imageLoadAddress);
             }
             else {
-                stacktrace << "- " << binary;
+                stacktrace.append("- {}", binary);
             }
 
             if (!function.empty()) {
-                stacktrace << " (" << function << " + " << offset << ")";
+                stacktrace.append(" ({} + 0x{:x})", function, offset);
             }
 
-            stacktrace << " @ " << std::showbase << std::hex << address << std::dec << "\n";
+            stacktrace.append(" @ 0x{:x}\n", address);
         }
     }
 
@@ -402,47 +409,46 @@ static std::string getStacktrace() {
 }
 
 static std::string getRegisters() {
-    std::stringstream registers;
+    StringBuffer<> registers;
 
     auto context = s_context;
     auto& ss = context->uc_mcontext->__ss;
 
     // geez
-    registers << std::showbase << std::hex /*<< std::setfill('0') << std::setw(16) */;
-    registers << "x0: " << ss.__x[0] << "\n";
-    registers << "x1: " << ss.__x[1] << "\n";
-    registers << "x2: " << ss.__x[2] << "\n";
-    registers << "x3: " << ss.__x[3] << "\n";
-    registers << "x4: " << ss.__x[4] << "\n";
-    registers << "x5: " << ss.__x[5] << "\n";
-    registers << "x6: " << ss.__x[6] << "\n";
-    registers << "x7: " << ss.__x[7] << "\n";
-    registers << "x8: " << ss.__x[8] << "\n";
-    registers << "x9: " << ss.__x[9] << "\n";
-    registers << "x10: " << ss.__x[10] << "\n";
-    registers << "x11: " << ss.__x[11] << "\n";
-    registers << "x12: " << ss.__x[12] << "\n";
-    registers << "x13: " << ss.__x[13] << "\n";
-    registers << "x14: " << ss.__x[14] << "\n";
-    registers << "x15: " << ss.__x[15] << "\n";
-    registers << "x16: " << ss.__x[16] << "\n";
-    registers << "x17: " << ss.__x[17] << "\n";
-    registers << "x18: " << ss.__x[18] << "\n";
-    registers << "x19: " << ss.__x[19] << "\n";
-    registers << "x20: " << ss.__x[20] << "\n";
-    registers << "x21: " << ss.__x[21] << "\n";
-    registers << "x22: " << ss.__x[22] << "\n";
-    registers << "x23: " << ss.__x[23] << "\n";
-    registers << "x24: " << ss.__x[24] << "\n";
-    registers << "x25: " << ss.__x[25] << "\n";
-    registers << "x26: " << ss.__x[26] << "\n";
-    registers << "x27: " << ss.__x[27] << "\n";
-    registers << "x28: " << ss.__x[28] << "\n";
-    registers << "fp: " << ss.__fp << "\n";
-    registers << "lr: " << ss.__lr << "\n";
-    registers << "sp: " << ss.__sp << "\n";
-    registers << "pc: " << ss.__pc << "\n";
-    registers << "cpsr: " << ss.__cpsr << "\n";
+    registers.append("x0: 0x{:x}\n", ss.__x[0]);
+    registers.append("x1: 0x{:x}\n", ss.__x[1]);
+    registers.append("x2: 0x{:x}\n", ss.__x[2]);
+    registers.append("x3: 0x{:x}\n", ss.__x[3]);
+    registers.append("x4: 0x{:x}\n", ss.__x[4]);
+    registers.append("x5: 0x{:x}\n", ss.__x[5]);
+    registers.append("x6: 0x{:x}\n", ss.__x[6]);
+    registers.append("x7: 0x{:x}\n", ss.__x[7]);
+    registers.append("x8: 0x{:x}\n", ss.__x[8]);
+    registers.append("x9: 0x{:x}\n", ss.__x[9]);
+    registers.append("x10: 0x{:x}\n", ss.__x[10]);
+    registers.append("x11: 0x{:x}\n", ss.__x[11]);
+    registers.append("x12: 0x{:x}\n", ss.__x[12]);
+    registers.append("x13: 0x{:x}\n", ss.__x[13]);
+    registers.append("x14: 0x{:x}\n", ss.__x[14]);
+    registers.append("x15: 0x{:x}\n", ss.__x[15]);
+    registers.append("x16: 0x{:x}\n", ss.__x[16]);
+    registers.append("x17: 0x{:x}\n", ss.__x[17]);
+    registers.append("x18: 0x{:x}\n", ss.__x[18]);
+    registers.append("x19: 0x{:x}\n", ss.__x[19]);
+    registers.append("x20: 0x{:x}\n", ss.__x[20]);
+    registers.append("x21: 0x{:x}\n", ss.__x[21]);
+    registers.append("x22: 0x{:x}\n", ss.__x[22]);
+    registers.append("x23: 0x{:x}\n", ss.__x[23]);
+    registers.append("x24: 0x{:x}\n", ss.__x[24]);
+    registers.append("x25: 0x{:x}\n", ss.__x[25]);
+    registers.append("x26: 0x{:x}\n", ss.__x[26]);
+    registers.append("x27: 0x{:x}\n", ss.__x[27]);
+    registers.append("x28: 0x{:x}\n", ss.__x[28]);
+    registers.append("fp: 0x{:x}\n", ss.__fp);
+    registers.append("lr: 0x{:x}\n", ss.__lr);
+    registers.append("sp: 0x{:x}\n", ss.__sp);
+    registers.append("pc: 0x{:x}\n", ss.__pc);
+    registers.append("cpsr: 0x{:x}\n", ss.__cpsr);
 
     return registers.str();
 }
