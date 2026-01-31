e2b385595af330ec9f384101b3ef699f2280e157
undefined06855 <currentcashew57@gmail.com>
2026-01-31 20:13:04 +0000

    make loadingspinner a ccnodergba, add a setContentSize overload

--- loader/include/Geode/ui/LoadingSpinner.hpp
+++ loader/include/Geode/ui/LoadingSpinner.hpp
@@ -9,7 +9,7 @@ namespace geode {
      * content size and is a lot more stripped down (not requiring a `show`
      * method or anything - it just works!)
      */
-    class GEODE_DLL LoadingSpinner : public cocos2d::CCNode {
+    class GEODE_DLL LoadingSpinner : public cocos2d::CCNodeRGBA {
     protected:
         cocos2d::CCSprite* m_spinner;
 
@@ -25,5 +25,6 @@ namespace geode {
         static LoadingSpinner* create(float size);
 
         void setVisible(bool visible) override;
+        void setContentSize(const cocos2d::CCSize& size) override;
     };
 }
--- loader/src/ui/nodes/LoadingSpinner.cpp
+++ loader/src/ui/nodes/LoadingSpinner.cpp
@@ -10,6 +10,7 @@ bool LoadingSpinner::init(float sideLength) {
     this->setID("loading-spinner");
     this->setContentSize({ sideLength, sideLength });
     this->setAnchorPoint({ .5f, .5f });
+    this->setCascadeOpacityEnabled(true);
 
     m_spinner = CCSprite::create("loadingCircle.png");
     m_spinner->setBlendFunc({ GL_ONE, GL_ONE });
@@ -22,7 +23,11 @@ bool LoadingSpinner::init(float sideLength) {
 }
 
 void LoadingSpinner::spin() {
-    m_spinner->runAction(CCRepeatForever::create(CCRotateBy::create(1.f, 360.f)));
+    auto action = CCRepeatForever::create(CCRotateBy::create(1.f, 360.f));
+    action->setTag(1);
+
+    m_spinner->stopActionByTag(1);
+    m_spinner->runAction(action);
 }
 
 LoadingSpinner* LoadingSpinner::create(float sideLength) {
@@ -40,4 +45,9 @@ void LoadingSpinner::setVisible(bool visible) {
     if (visible) {
         this->spin();
     }
+}
+
+void LoadingSpinner::setContentSize(const CCSize& size) {
+    CCNode::setContentSize(size);
+    if (m_spinner) limitNodeSize(m_spinner, m_obContentSize, 1.f, .1f);
 }
