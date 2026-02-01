ec9098bcc78d59734f15e1021e01a46a9e9d2d27
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2026-02-01 12:21:19 -0500

    Templated ZStringView

--- loader/include/Geode/utils/ZStringView.hpp
+++ loader/include/Geode/utils/ZStringView.hpp
@@ -7,33 +7,36 @@
 #include <fmt/format.h>
 
 namespace geode {
-    class ZStringView {
+    template <typename C>
+    class BasicZStringView {
     public:
-        ZStringView() : m_str("") {}
-        ZStringView(char const* str) : m_str(str ? str : "") {}
-        ZStringView(std::string const& str) : m_str(str.c_str()), m_length(str.size()) {}
+        using value_type = C;
+
+        BasicZStringView() : m_str("") {}
+        BasicZStringView(C const* str) : m_str(str ? str : "") {}
+        BasicZStringView(std::basic_string<C> const& str) : m_str(str.c_str()), m_length(str.size()) {}
 
 #ifdef GEODE_IS_ANDROID
-        ZStringView(gd::string const& str) : m_str(str.c_str()), m_length(str.size()) {}
+        BasicZStringView(gd::string const& str) requires std::is_same_v<C, char> : m_str(str.c_str()), m_length(str.size()) {}
 #endif
 
-        ZStringView(ZStringView const& other) = default;
-        ZStringView& operator=(ZStringView const& other) = default;
+        BasicZStringView(BasicZStringView const& other) = default;
+        BasicZStringView& operator=(BasicZStringView const& other) = default;
 
-        explicit operator char const*() const {
+        explicit operator C const*() const {
             return m_str;
         }
 
-        operator std::string_view() const {
+        operator std::basic_string_view<C>() const {
             return this->view();
         }
 
-        operator std::string() const {
-            return std::string(m_str, _size());
+        operator std::basic_string<C>() const {
+            return std::basic_string<C>(m_str, _size());
         }
 
 #ifdef GEODE_IS_ANDROID
-        operator gd::string() const {
+        operator gd::string() const requires std::is_same_v<C, char> {
             return gd::string(m_str, _size());
         }
 #endif
@@ -42,16 +45,16 @@ namespace geode {
             return std::filesystem::path(this->view());
         }
 
-        char const* c_str() const {
+        C const* c_str() const {
             return m_str;
         }
 
-        char const* data() const {
+        C const* data() const {
             return m_str;
         }
 
-        std::string_view view() const {
-            return std::string_view(m_str, _size());
+        std::basic_string_view<C> view() const {
+            return std::basic_string_view<C>(m_str, _size());
         }
 
         size_t size() const {
@@ -74,58 +77,64 @@ namespace geode {
             return m_str + _size();
         }
 
-        auto operator<=>(ZStringView const& other) const {
-            return std::string_view(*this) <=> std::string_view(other);
+        auto operator<=>(BasicZStringView const& other) const {
+            return std::basic_string_view<C>(*this) <=> std::basic_string_view<C>(other);
         }
 
-        bool operator==(ZStringView const& other) const {
-            return std::string_view(*this) == std::string_view(other);
+        bool operator==(BasicZStringView const& other) const {
+            return std::basic_string_view<C>(*this) == std::basic_string_view<C>(other);
         }
 
-        bool operator==(char const* other) const {
-            return std::string_view(*this) == std::string_view(other);
+        bool operator==(C const* other) const {
+            return std::basic_string_view<C>(*this) == std::basic_string_view<C>(other);
         }
 
     private:
-        char const* m_str;
+        C const* m_str;
         mutable size_t m_length = (size_t)-1;
 
         size_t _size() const {
             if (m_length == (size_t)-1) {
-                m_length = std::char_traits<char>::length(m_str);
+                m_length = std::char_traits<C>::length(m_str);
             }
 
             return m_length;
         }
         
-        friend std::ostream& operator<<(std::ostream& os, ZStringView const& zsv) {
+        friend std::basic_ostream<C>& operator<<(std::basic_ostream<C>& os, BasicZStringView const& zsv) {
             os << zsv.view();
             return os;
         }
     };
 
-    template <typename T>
-    requires std::same_as<std::remove_cvref_t<T>, ZStringView>
+    using ZStringView = BasicZStringView<char>;
+    using WZStringView = BasicZStringView<wchar_t>;
+    using U8ZStringView = BasicZStringView<char8_t>;
+    using U16ZStringView = BasicZStringView<char16_t>;
+    using U32ZStringView = BasicZStringView<char32_t>;
+
+    template <typename T, typename C>
+    requires std::same_as<std::remove_cvref_t<T>, BasicZStringView<C>>
     std::filesystem::path operator/(std::filesystem::path const& lhs, T&& rhs) {
-        return lhs / std::string_view{rhs};
+        return lhs / rhs.view();
     }
     
-    template <typename T>
-    requires std::same_as<std::remove_cvref_t<T>, ZStringView>
-    std::string operator+(std::string const& lhs, T&& rhs) {
-        std::string out;
-        out.reserve(lhs.size() + std::string_view{rhs}.size());
+    template <typename T, typename C>
+    requires std::same_as<std::remove_cvref_t<T>, BasicZStringView<C>>
+    std::basic_string<C> operator+(std::basic_string<C> const& lhs, T&& rhs) {
+        std::basic_string<C> out;
+        out.reserve(lhs.size() + rhs.size());
         out += lhs;
-        out += std::string_view{rhs};
+        out += rhs.view();
         return out;
     }
     
-    template <typename T>
-    requires std::same_as<std::remove_cvref_t<T>, ZStringView>
-    std::string operator+(T&& lhs, std::string const& rhs) {
-        std::string out;
-        out.reserve(std::string_view{lhs}.size() + rhs.size());
-        out += std::string_view{lhs};
+    template <typename T, typename C>
+    requires std::same_as<std::remove_cvref_t<T>, BasicZStringView<C>>
+    std::basic_string<C> operator+(T&& lhs, std::basic_string<C> const& rhs) {
+        std::basic_string<C> out;
+        out.reserve(lhs.size() + rhs.size());
+        out += lhs.view();
         out += rhs;
         return out;
     }
