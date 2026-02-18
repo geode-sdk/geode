db160d85cf2ac42d42d1fae63aa21e8bf6a480a4
HJfod <60038575+HJfod@users.noreply.github.com>
2026-02-18 09:53:49 +0200

    hook gd keybindings popup

---
+++ loader/src/hooks/KeybindHooks.cpp
@@ -0,0 +1,12 @@
+#include <Geode/modify/MoreOptionsLayer.hpp>
+#include <ui/mods/settings/KeybindsPopup.hpp>
+
+using namespace geode::prelude;
+
+// This seems to also be merged with the editor keybindings button
+class $modify(MoreOptionsLayer) {
+	$override
+	void onKeybindings(CCObject*) {
+		KeybindsPopup::create(true)->show();
+	}
+};
