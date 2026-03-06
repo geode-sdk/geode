f0470f8131bef61c58341482abbf0fc55409e15e
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2026-03-06 15:39:07 -0500

    Fix game termination saving on macOS

--- loader/src/hooks/ShutdownFix.cpp
+++ loader/src/hooks/ShutdownFix.cpp
@@ -1,5 +1,6 @@
 #include <Geode/platform/cplatform.h>
 #ifdef GEODE_IS_MACOS
+#include <Geode/binding/PlatformToolbox.hpp>
 #include <Geode/loader/Mod.hpp>
 #include <Geode/utils/ObjcHook.hpp>
 #include <objc/message.h>
@@ -18,11 +19,26 @@ void shutdownGameHook(void* self, SEL sel) {
     reinterpret_cast<void(*)(void*, SEL)>(objc_msgSend)(self, sel);
 }
 
+// This fixes the game saving twice on exit,
+// which can cause issues with mods that handle saving before shutdown.
+
+void applicationShouldTerminateExecHook(void* self, SEL sel) {
+    *reinterpret_cast<bool*>(reinterpret_cast<uintptr_t>(self) + 0x19) = true;
+    PlatformToolbox::gameDidSave();
+}
+
 $execute {
     if (auto hook = ObjcHook::create(
         "AppController",
         "shutdownGame",
         &shutdownGameHook
+    )) {
+        (void) Mod::get()->claimHook(hook.unwrap());
+    }
+    if (auto hook = ObjcHook::create(
+        "AppController",
+        "applicationShouldTerminateExec",
+        &applicationShouldTerminateExecHook
     )) {
         (void) Mod::get()->claimHook(hook.unwrap());
     }
