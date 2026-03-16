2262a8e04d16be27e2eacc37efcbcd5b8d8f6171
Cvolton <cvolton@cvolton.eu>
2026-03-16 03:47:01 +0100

    update dll error messages to better use the mdpopup space

--- loader/src/platform/windows/ModImpl.cpp
+++ loader/src/platform/windows/ModImpl.cpp
@@ -17,37 +17,37 @@ T findSymbolOrMangled(HMODULE load, char const* name, char const* mangled) {
 char const* getUsefulError(DWORD code) {
     switch (code) {
         case ERROR_MOD_NOT_FOUND:
-            return "ERROR_MOD_NOT_FOUND; The mod is either missing the DLL "
+            return "ERROR_MOD_NOT_FOUND;\n\nThe mod is either missing the DLL "
                    "file or some of its dependencies. Make sure to list all "
                    "other mods you depend on under dependencies in mod.json  "
                    "and that the mod is not built in Debug. "
-                   "If you are not the developer of this mod, report this error "
-                   "to them as it is likely not your fault. Also try updating "
-                   "vcredist.";
+                   "\n\nIf you are not the developer of this mod, report this error "
+                   "to them as it is likely not your fault. Also try [updating "
+                   "Microsoft Visual C++ Redistributable 2026 (64-bit).](https://aka.ms/vc14/vc_redist.x64.exe)";
 
         case ERROR_PROC_NOT_FOUND:
-            return "ERROR_PROC_NOT_FOUND; The mod tried to access "
+            return "ERROR_PROC_NOT_FOUND;\n\nThe mod tried to access "
                    "a function defined in another DLL, but the specified "
                    "function was not found. Make sure the other DLL exports "
-                   "the given symbol, and that it is defined in the DLL. "
+                   "the given symbol, and that it is defined in the DLL.\n\n"
                    "If you are not the developer of this mod, report this error "
                    "to them as it is likely not your fault.";
 
         case ERROR_DLL_INIT_FAILED:
-            return "ERROR_DLL_INIT_FAILED; Likely some global variables "
+            return "ERROR_DLL_INIT_FAILED;\n\nLikely some global variables "
                    "in the mod threw an exception or otherwise failed. "
-                   "ALSO MAKE SURE YOU ARE COMPILING IN RELEASE MODE. "
+                   "ALSO MAKE SURE YOU ARE COMPILING IN RELEASE MODE.\n\n"
                    "If you are not the developer of this mod, report this error "
                    "to them as it is likely not your fault.";
 
         case 4551:
-            return "Blocked by Windows Smart App Control (4551); The mod was blocked "
+            return "Blocked by Windows Smart App Control (4551);\n\nThe mod was blocked "
                     "from loading by Windows Smart App Control. This is a security feature "
                     "in Windows that blocks unrecognized applications including most "
                     "Geometry Dash mods.";
 
         case 0xc0e90002:
-            return "Blocked by Windows Smart App Control (0xc0e90002); The mod was blocked "
+            return "Blocked by Windows Smart App Control (0xc0e90002);\n\nThe mod was blocked "
                     "from loading by Windows Smart App Control. This is a security feature "
                     "in Windows that blocks unrecognized applications including most "
                     "Geometry Dash mods.";
