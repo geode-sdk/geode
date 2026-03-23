4f7910da33d1f10bd522c86696feb50533cd666b
Cvolton <cvolton@cvolton.eu>
2026-03-23 14:19:54 +0100

    dont save settings if theyre all default

--- loader/src/loader/ModImpl.cpp
+++ loader/src/loader/ModImpl.cpp
@@ -238,13 +238,19 @@ Result<> Mod::Impl::saveData() {
     // ModSettingsManager keeps track of the whole savedata
     matjson::Value json = m_settings->save();
 
+    bool isDefault = true;
+    for (const auto& [key, value] : m_saved) {
+        if (m_settings->get(key) && m_settings->get(key)->isDefaultValue()) {
+            isDefault = false;
+            break;
+        }
+    }
+
     // saveData is expected to be synchronous, and always called from GD thread
     ModStateEvent(ModEventType::DataSaved, std::move(m_self)).send();
 
-    // TODO: better check if its empty?
-    auto dump = json.dump();
-    if(!dump.starts_with("{}")) {
-        auto res = utils::file::writeStringSafe(m_saveDirPath / "settings.json", dump);
+    if(!isDefault) {
+        auto res = utils::file::writeStringSafe(m_saveDirPath / "settings.json", json.dump());
         if (!res) {
             log::error("Unable to save settings: {}", res.unwrapErr());
         }
@@ -256,7 +262,8 @@ Result<> Mod::Impl::saveData() {
         }
     }
 
-    dump = m_saved.dump();
+    // TODO: better check if its empty?
+    auto dump = m_saved.dump();
     if (!dump.starts_with("{}")) {
         auto res2 = utils::file::writeStringSafe(m_saveDirPath / "saved.json", m_saved.dump());
         if (!res2) {
