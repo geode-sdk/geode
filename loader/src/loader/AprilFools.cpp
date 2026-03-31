53bb982329edd81acd41f13c3d86cfe922e4f964
altalk23 <45172705+altalk23@users.noreply.github.com>
2026-04-01 00:02:59 +0300

    add caching to node traversal on mod dependency graph

--- loader/src/loader/AprilFools.cpp
+++ loader/src/loader/AprilFools.cpp
@@ -1,23 +1,18 @@
 #include <Geode/Geode.hpp>
+#include "ui/mods/ModsLayer.hpp"
 
 using namespace geode::prelude;
 
-static bool isAprilFools() {
-    auto now = std::chrono::system_clock::now();
-    auto timeNow = std::chrono::system_clock::to_time_t(now);
-    auto tm_local = asp::localtime(timeNow);
-
-    return tm_local.tm_mon == 3 && tm_local.tm_mday == 1;
-}
-
 namespace sapphire {
-    class CountdownTimer : public CCLabelBMFont {
+    class CountdownTimer : public CCNode {
     public:
+        CCLabelBMFont* m_timer;
+
         static CountdownTimer* create() {
             auto ret = new CountdownTimer();
-            if (ret->initWithString("", "chatFont.fnt", kCCLabelAutomaticWidth, kCCTextAlignmentRight)) {
+            if (ret->init()) {
                 ret->autorelease();
-                ret->schedule(schedule_selector(CountdownTimer::updateTimer), 1.f);
+                ret->schedule(schedule_selector(CountdownTimer::updateTimer), 0.f);
                 ret->updateTimer(0.f);
                 return ret;
             }
@@ -25,7 +20,42 @@ namespace sapphire {
             return nullptr;
         }
 
+        bool init() {
+            this->setLayout(SimpleColumnLayout::create()->setCrossAxisAlignment(CrossAxisAlignment::Start)->setGap(5.f));
+            this->setContentSize(CCSize{250, 50});
+
+            auto activate = CCLabelBMFont::create("Activate Sapphire Pro", "chatFont.fnt", kCCLabelAutomaticWidth, kCCTextAlignmentRight);
+            activate->setScale(1.2f);
+            activate->setColor({ 255, 255, 255 });
+            activate->setOpacity(200);
+            this->addChild(activate);
+
+            auto sapphirePro = CCLabelBMFont::create("Go to Settings to activate Sapphire Pro.", "chatFont.fnt", kCCLabelAutomaticWidth, kCCTextAlignmentRight);
+            sapphirePro->setScale(0.8f);
+            sapphirePro->setColor({ 255, 255, 255 });
+            sapphirePro->setOpacity(200);
+            this->addChild(sapphirePro);
+
+            m_timer = CCLabelBMFont::create("", "chatFont.fnt", kCCLabelAutomaticWidth, kCCTextAlignmentRight);
+            m_timer->setScale(0.8f);
+            m_timer->setColor({ 255, 255, 255 });
+            m_timer->setOpacity(200);
+            this->addChild(m_timer);
+
+            this->updateLayout();
+
+            return true;
+        }
+
         void updateTimer(float dt) {
+            if (MenuLayer::get() || CCScene::get()->getChildByType<ModsLayer*>(0)) {
+                this->setVisible(true);
+            }
+            else {
+                this->setVisible(false);
+            }
+            if (!this->isVisible()) return;
+
             static auto aprilSecond = []() {
                 auto now = std::chrono::system_clock::now();
                 auto timeNow = std::chrono::system_clock::to_time_t(now);
@@ -44,15 +74,19 @@ namespace sapphire {
             auto difference = aprilSecond - now;
 
             if (difference.has_value()) {
-                auto minutes = static_cast<int>(difference->minutes());
+                auto hours = static_cast<int>(difference->hours());
+                auto minutes = static_cast<int>(difference->minutes() % 60);
                 auto seconds = static_cast<int>(difference->seconds() % 60);
-                this->setString(
+                m_timer->setString(
                     fmt::format(
-                        "Sapphire Pro Trial Edition\nRemaining time: {:02}:{:02}",
-                        minutes, seconds
+                        "Evaluation period: {:02}:{:02}:{:02}",
+                        hours, minutes, seconds
                     ).c_str()
                 );
             } else {
+                Mod::get()->getSaveContainer().erase("sapphire-style");
+                Mod::get()->getSaveContainer().erase("show-april-popup-2026");
+
                 FLAlertLayer::create(
                     "Happy April Fools!",
                     "Don't worry, Geode SDK will forever remain free for everyone :D",
@@ -61,14 +95,24 @@ namespace sapphire {
 
                 this->removeFromParentAndCleanup(true);
             }
+
+            this->updateLayout();
         }
     };
 }
 
 
 $on_game(Loaded) {
-    // Mod::get()->setSavedValue("sapphire-style", true);
-    if (!isAprilFools()) return;
+    if (Mod::get()->getSavedValue("show-april-popup-2026", false)) {
+        Mod::get()->getSaveContainer().erase("sapphire-style");
+        Mod::get()->getSaveContainer().erase("show-april-popup-2026");
+        FLAlertLayer::create(
+            "Happy April Fools!",
+            "Don't worry, Geode SDK will forever remain free for everyone :D",
+            "OK"
+        )->show();
+    }
+
     if (!Mod::get()->getSavedValue("sapphire-style", false)) return;
 
     FLAlertLayer::create(
@@ -81,10 +125,11 @@ $on_game(Loaded) {
 
     auto timer = sapphire::CountdownTimer::create();
     if (!timer) return;
+    auto winsize = CCDirector::get()->getWinSize();
 
+    timer->setAnchorPoint({1.f, 0.f});
     timer->setScale(0.75f);
-    timer->setAnchorPoint({1.f, 1.f});
-    timer->setPosition(CCDirector::get()->getWinSize() - CCSize{5.f, 5.f});
+    timer->setPosition(CCSize{winsize.width - 10.f, 10.f});
 
     OverlayManager::get()->addChild(timer);
 }
