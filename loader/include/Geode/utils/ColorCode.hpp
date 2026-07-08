271566643c9fa260536109a5a66aa9e8571f6559
SMJS <sjoerd.t@live.nl>
2026-07-08 13:11:25 +0200

    Made the parsing return a result and added HSV support

--- loader/include/Geode/utils/ColorCode.hpp
+++ loader/include/Geode/utils/ColorCode.hpp
@@ -11,38 +11,65 @@ namespace geode {
         object.setOpacity(GLubyte());
     };
 
-    class GEODE_DLL ColorCode {
+    struct GEODE_DLL Color final {
+        /// Parses the given hex string using one of the following formats:
+        /// #R/G/B, #RR/GG/BB, #RGB, #RGBA, #RRGGBB or #RRGGBBAA
+        /// Where the # prefix is optional and / means it's 1 byte/nibble representing all channels
+        /// @returns The parsed result
+        static Result<Color> parse(std::string_view hex);
+    private:
+        inline static GLubyte getByte(uint32_t colorNum, size_t index, bool isShort);
     public:
         /// @returns The current alpha value
         operator float() const;
         operator cocos2d::ccColor3B() const;
         operator cocos2d::ccColor4B() const;
+        operator cocos2d::extension::HSV() const;
+        Color& operator=(Color&& other) = default;
+        Color& operator=(const Color& other) = default;
 
-        GLubyte m_r;
-        GLubyte m_g;
-        GLubyte m_b;
-        GLubyte m_a;
+        GLubyte r;
+        GLubyte g;
+        GLubyte b;
+        GLubyte a;
 
+        Color(Color&& other) = default;
+        Color(const Color& other) = default;
         /// @returns The equivalent of #000000{a}
-        ColorCode(GLubyte a = 0);
-        /// Parses the given hex string using one of the following formats:
-        /// #R/G/B, #RR/GG/BB, #RGB, #RGBA, #RRGGBB or #RRGGBBAA
-        /// Where the # prefix is optional and / means it's 1 byte/nibble representing all channels
-        /// @returns The parsed hex string
-        ColorCode(std::string_view hex);
-        /// @returns The equivalent of #{r}{g}{b}{a}
-        ColorCode(GLubyte r, const GLubyte g, const GLubyte b, const GLubyte a = 255);
+        Color(GLubyte alpha = 0);
+        Color(GLubyte red, GLubyte green, GLubyte blue, GLubyte alpha = 0xFF);
+        Color(const cocos2d::ccColor3B& color, GLubyte alpha = 0xFF);
+        Color(const cocos2d::ccColor4B& color);
+        Color(const cocos2d::extension::HSV& hsv, GLubyte alpha = 0xFF);
 
+        void applyH(double hue);
+        void applyHue(double hue);
+        void applyS(double saturation);
+        void applySaturation(double saturation);
+        void applyV(double value);
+        void applyValue(double value);
+        void applyB(double brightness);
+        void applyBrightness(double brightness);
+        void applyHSV(const cocos2d::extension::HSV& hsv);
+        void setR(GLubyte red);
         GLubyte getR() const;
+        void setRed(GLubyte red);
         GLubyte getRed() const;
+        void setG(GLubyte green);
         GLubyte getG() const;
+        void setGreen(GLubyte green);
         GLubyte getGreen() const;
+        void setB(GLubyte blue);
         GLubyte getB() const;
+        void setBlue(GLubyte blue);
         GLubyte getBlue() const;
+        void setA(GLubyte alpha);
         GLubyte getA() const;
+        void setAlpha(GLubyte alpha);
         GLubyte getAlpha() const;
         cocos2d::ccColor3B to3B() const;
         cocos2d::ccColor4B to4B() const;
+        cocos2d::extension::HSV toHSV() const;
         /// @returns True if alpha is 0
         bool isInvisible() const;
         /// @returns True if alpha is bigger than 0 and smaller than 255
@@ -58,8 +85,5 @@ namespace geode {
                 node->setOpacity(this->getA());
             }
         }
-    private:
-        void extractChannels(std::string_view hex);
-        GLubyte extractChannel(std::string_view hexString, size_t index, bool isShort) const;
     };
 }
--- loader/src/utils/ColorCode.cpp
+++ loader/src/utils/ColorCode.cpp
@@ -2,114 +2,257 @@
 
 using namespace geode::prelude;
 
-ColorCode::operator float() const {
-    return m_a;
-}
-
-ColorCode::operator ccColor3B() const {
-    return { m_r, m_g, m_b };
-}
-
-ColorCode::operator ccColor4B() const {
-    return { m_r, m_g, m_b, m_a };
-}
-
-ColorCode::ColorCode(GLubyte a) : m_r(0x00), m_g(0x00), m_b(0x00), m_a(a) { }
-
-ColorCode::ColorCode(std::string_view hex) : m_r(0x00), m_g(0x00), m_b(0x00), m_a(0x00) {
-    this->extractChannels(hex);
-}
-
-ColorCode::ColorCode(GLubyte r, GLubyte g, GLubyte b, GLubyte a) : m_r(r), m_g(g), m_b(b), m_a(a) { }
-
-GLubyte ColorCode::getR() const {
-    return m_r;
-}
-
-GLubyte ColorCode::getRed() const {
-    return m_r;
-}
-
-GLubyte ColorCode::getG() const {
-    return m_g;
-}
-
-GLubyte ColorCode::getGreen() const {
-    return m_g;
-}
-
-GLubyte ColorCode::getB() const {
-    return m_b;
-}
-
-GLubyte ColorCode::getBlue() const {
-    return m_b;
-}
-
-GLubyte ColorCode::getA() const {
-    return m_a;
-}
-
-GLubyte ColorCode::getAlpha() const {
-    return m_a;
-}
-
-cocos2d::ccColor3B ColorCode::to3B() const {
-    return *this;
-}
-
-cocos2d::ccColor4B ColorCode::to4B() const {
-    return *this;
-}
-
-
-bool ColorCode::isInvisible() const {
-    return m_a == 0x00;
-}
-
-bool ColorCode::isTransparent() const {
-    return m_a > 0x00 && m_a < 0xFF;
-}
-
-bool ColorCode::isOpaque() const {
-    return m_a == 0xFF;
-}
-
-void ColorCode::extractChannels(std::string_view hex) {
+Result<Color> Color::parse(std::string_view hex) {
     const bool isPrefixed = hex.starts_with("#");
 
-    if (hex.size() == isPrefixed) return;
+    if (hex.size() == isPrefixed) {
+        return Err("Empty color string");
+    }
 
     const std::string_view hexStr = hex.substr(isPrefixed);
     const size_t size = hexStr.size();
 
     // If the string is not hex and the size is not an exact size of 1, 2, 3, 4, 6 or 8
-    if (hexStr.find_first_not_of("0123456789ABCDEFabcdef") != std::string_view::npos || (size > 4 && size != 6 && size != 8)) return;
-
-    // If the size is 2 or 1 then all channels are equal and alpha is max
-    if (size <= 2) {
-        m_r = m_g = m_b = extractChannel(hexStr, 0, size == 1);
-        m_a = 0xFF;
-    } else {
-        const bool isShort = size < 6;
-
-        m_r = extractChannel(hexStr, 0, isShort);
-        m_g = extractChannel(hexStr, 1, isShort);
-        m_b = extractChannel(hexStr, 2, isShort);
-
-        // If a 4th byte or nibble exists, extract the alpha channel, otherwise default to full alpha
-        if (size % 4 == 0) {
-            m_a = extractChannel(hexStr, 3, isShort);
-        } else {
-            m_a = 0xFF;
-        }
+    if (hexStr.find_first_not_of("0123456789ABCDEFabcdef") != std::string_view::npos) {
+        return Err("Non base16 characters");
+    } else if (size > 4 && size != 6 && size != 8) {
+        return Err("Unsupported size");
     }
+
+    GEODE_UNWRAP_INTO(const uint32_t colorNum, utils::numFromString<uint32_t>(hexStr, 16));
+    ccColor4B color;
+
+    if (size <= 2) {
+        color.r = color.g = color.b = Color::getByte(colorNum, 0, size == 1);
+        color.a = 0xFF;
+    } else if (size % 3 == 0) {
+        const bool isShort = size == 3;
+
+        color.r = Color::getByte(colorNum, 2, isShort);
+        color.g = Color::getByte(colorNum, 1, isShort);
+        color.b = Color::getByte(colorNum, 0, isShort);
+        color.a = 0xFF;
+    } else {
+        const bool isShort = size == 4;
+
+        color.r = Color::getByte(colorNum, 3, isShort);
+        color.g = Color::getByte(colorNum, 2, isShort);
+        color.b = Color::getByte(colorNum, 1, isShort);
+        color.a = Color::getByte(colorNum, 0, isShort);
+    }
+
+    return Ok(Color(color));
 }
 
-GLubyte ColorCode::extractChannel(std::string_view hexString, size_t index, bool isShort) const {
-    const size_t channelSize = 1 + !isShort;
-    const GLubyte byte = utils::numFromString<GLubyte>(hexString.substr(channelSize * index, channelSize), 16).unwrapOr(0);
+GLubyte Color::getByte(uint32_t colorNum, size_t index, bool isShort) {
+    const GLubyte byte = colorNum >> (isShort ? 4 : 8) * index & 0xFF;
 
     // If it's short, mirror the nibble
     return isShort ? (byte << 4) | byte : byte;
+}
+
+
+Color::operator float() const {
+    return a;
+}
+
+Color::operator ccColor3B() const {
+    return { r, g, b };
+}
+
+Color::operator ccColor4B() const {
+    return { r, g, b, a };
+}
+
+Color::operator HSV() const {
+    HSV hsv;
+    double red = r / 255.0;
+    double green = g / 255.0;
+    double blue = b / 255.0;
+    double max = std::max({ red, green, blue });
+    double delta = max - std::min({ red, green, blue });
+
+    if (delta == 0) {
+        hsv.h = 0;
+    } else if (max == red) {
+        hsv.h = 60 * std::fmod((green - blue) / delta, 6);
+    } else if (max == green) {
+        hsv.h = 60 * ((blue - red) / delta + 2);
+    } else {
+        hsv.h = 60 * ((red - green) / delta + 4);
+    }
+
+    if (hsv.h < 0) {
+        hsv.h += 360;
+    }
+
+    hsv.s = max == 0 ? 0 : delta / max;
+    hsv.v = max;
+
+    return hsv;
+}
+
+Color::Color(GLubyte alpha): r(0x00), g(0x00), b(0x00), a(alpha) { }
+
+Color::Color(GLubyte red, GLubyte green, GLubyte blue, GLubyte alpha): r(red), g(green), b(blue), a(alpha) { }
+
+Color::Color(const ccColor3B& color, GLubyte alpha): r(color.r), g(color.g), b(color.b), a(alpha) { }
+
+Color::Color(const ccColor4B& color): r(color.r), g(color.g), b(color.b), a(color.a) { }
+
+Color::Color(const HSV& hsv, GLubyte alpha): a(alpha) {
+    double c = hsv.v * hsv.s;
+    double x = c * (1 - std::abs(std::fmod(hsv.h / 60, 2) - 1));
+    double m = hsv.v - c;
+    double rp, gp, bp;
+
+    if (hsv.h < 60) {
+        rp = c; gp = x; bp = 0;
+    } else if (hsv.h < 120) {
+        rp = x; gp = c; bp = 0;
+    } else if (hsv.h < 180) {
+        rp = 0; gp = c; bp = x;
+    } else if (hsv.h < 240) {
+        rp = 0; gp = x; bp = c;
+    } else if (hsv.h < 300) {
+        rp = x; gp = 0; bp = c;
+    } else {
+        rp = c; gp = 0; bp = x;
+    }
+
+    r = std::round((rp + m) * 255.0);
+    g = std::round((gp + m) * 255.0);
+    b = std::round((bp + m) * 255.0);
+}
+
+void Color::applyH(double hue) {
+    this->applyHSV({ hue, 1, 1 });
+}
+
+void Color::applyHue(double hue) {
+    this->applyHSV({ hue, 1, 1 });
+}
+
+void Color::applyS(double saturation) {
+    this->applyHSV({ 0, saturation, 1 });
+}
+
+void Color::applySaturation(double saturation) {
+    this->applyHSV({ 0, saturation, 1 });
+}
+
+void Color::applyV(double value) {
+    this->applyHSV({ 0, 1, value });
+}
+
+void Color::applyValue(double value) {
+    this->applyHSV({ 0, 1, value });
+}
+
+void Color::applyB(double brightness) {
+    this->applyHSV({ 0, 1, brightness });
+}
+
+void Color::applyBrightness(double brightness) {
+    this->applyHSV({ 0, 1, brightness });
+}
+
+void Color::applyHSV(const HSV& hsv) {
+    HSV currentHSV = this->toHSV();
+
+    currentHSV.h = std::fmod(currentHSV.h + std::fmod(hsv.h, 360) + 360, 360);
+    currentHSV.s = std::clamp<float>(currentHSV.s * hsv.s, 0, 1);
+    currentHSV.v = std::clamp<float>(currentHSV.v * hsv.v, 0, 1);
+
+    *this = currentHSV;
+}
+
+
+
+void Color::setR(GLubyte red) {
+    r = red;
+}
+
+GLubyte Color::getR() const {
+    return r;
+}
+
+void Color::setRed(GLubyte red) {
+    r = red;
+}
+
+GLubyte Color::getRed() const {
+    return r;
+}
+
+void Color::setG(GLubyte green) {
+    g = green;
+}
+
+GLubyte Color::getG() const {
+    return g;
+}
+
+void Color::setGreen(GLubyte green) {
+    g = green;
+}
+
+GLubyte Color::getGreen() const {
+    return g;
+}
+
+void Color::setB(GLubyte blue) {
+    b = blue;
+}
+
+GLubyte Color::getB() const {
+    return b;
+}
+
+void Color::setBlue(GLubyte blue) {
+    b = blue;
+}
+
+GLubyte Color::getBlue() const {
+    return b;
+}
+
+void Color::setA(GLubyte alpha) {
+    a = alpha;
+}
+
+GLubyte Color::getA() const {
+    return a;
+}
+
+void Color::setAlpha(GLubyte alpha) {
+    a = alpha;
+}
+
+GLubyte Color::getAlpha() const {
+    return a;
+}
+
+ccColor3B Color::to3B() const {
+    return *this;
+}
+
+ccColor4B Color::to4B() const {
+    return *this;
+}
+
+HSV Color::toHSV() const {
+    return *this;
+}
+
+bool Color::isInvisible() const {
+    return a == 0x00;
+}
+
+bool Color::isTransparent() const {
+    return a > 0x00 && a < 0xFF;
+}
+
+bool Color::isOpaque() const {
+    return a == 0xFF;
 }
