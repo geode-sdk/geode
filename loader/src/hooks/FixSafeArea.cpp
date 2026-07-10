8cc197b884904d6a384e599779c7354f980d90f0
Chloe <25387744+qimiko@users.noreply.github.com>
2026-07-10 16:32:51 -0700

    Update FixSafeArea.cpp

--- loader/src/hooks/FixSafeArea.cpp
+++ loader/src/hooks/FixSafeArea.cpp
@@ -9,7 +9,7 @@ class $modify(FixSafeAreaAppDelegate, AppDelegate) {
     void setupGLView() {
         AppDelegate::setupGLView();
         // just detect if the left inset exists (might be good to change to >= some value but i'm too lazy to see what that value is)
-        m_needsSafeArea = geode::utils::getSafeAreaRect().x > 0.0f;
+        m_needsSafeArea = geode::utils::getSafeAreaRect().origin.x > 0.0f;
     }
 };
 #endif
