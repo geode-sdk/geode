02113b00837557e0441a06c5c3d85061f7a582fe
slideglide <68702247+slideglide@users.noreply.github.com>
2026-09-05 17:37:18 +0300

    remove fenced code blocks (vs code doesn't show them, flash will)

--- loader/include/Geode/ui/Toggler.hpp
+++ loader/include/Geode/ui/Toggler.hpp
@@ -18,11 +18,9 @@ namespace geode {
         /// @param toggleCallback Called when the toggle changes.
         ///
         /// @example
-        /// ```
         /// auto toggler = Toggler::create([](Toggler* sender, bool isToggled) {
         ///     log::info("Toggled: {}", isToggled);
         /// });
-        /// ```
         static Toggler* create(TogglerCallback toggleCallback = nullptr);
 
         /// Make a toggle with custom on/off nodes.
@@ -32,7 +30,6 @@ namespace geode {
         /// @param toggleCallback Called when the toggle changes.
         ///
         /// @example
-        /// ```
         /// auto offNode = CCSprite::createWithSpriteFrameName("GJ_checkOff_001.png");
         /// auto onNode = CCSprite::createWithSpriteFrameName("GJ_checkOn_001.png");
         ///
@@ -43,7 +40,6 @@ namespace geode {
         ///         log::info("Toggled: {}", isToggled);
         ///     }
         /// );
-        /// ```
         static Toggler* createWithNodes(
             cocos2d::CCNode* offNode,
             cocos2d::CCNode* onNode,
@@ -56,14 +52,12 @@ namespace geode {
         /// @param scale Checkbox scale.
         ///
         /// @example
-        /// ```
         /// auto toggler = Toggler::createWithStandardSprites(
         ///     [](Toggler* sender, bool isToggled) {
         ///         log::info("Checkbox: {}", isToggled);
         ///     },
         ///     0.8f
         /// );
-        /// ```
         static Toggler* createWithStandardSprites(
             TogglerCallback toggleCallback = nullptr,
             float scale = 1.0f
@@ -77,7 +71,6 @@ namespace geode {
         /// @param scale Sprite scale.
         ///
         /// @example
-        /// ```
         /// auto toggler = Toggler::createWithSprites(
         ///     "off.png"_spr,
         ///     "on.png"_spr,
@@ -86,7 +79,6 @@ namespace geode {
         ///     },
         ///     1.25f
         /// );
-        /// ```
         static Toggler* createWithSprites(
             geode::ZStringView offFileName,
             geode::ZStringView onFileName,
@@ -102,7 +94,6 @@ namespace geode {
         /// @param scale Sprite scale.
         ///
         /// @example
-        /// ```
         /// auto toggler = Toggler::createWithSpriteFrameNames(
         ///     "GJ_checkOff_001.png"_spr,
         ///     "GJ_checkOn_001.png"_spr,
@@ -110,7 +101,6 @@ namespace geode {
         ///         log::info("Checkbox state changed: {}", isToggled);
         ///     }
         /// );
-        /// ```
         static Toggler* createWithSpriteFrameNames(
             geode::ZStringView offFrameName,
             geode::ZStringView onFrameName,
@@ -121,11 +111,9 @@ namespace geode {
         /// Check if the toggle is on.
         ///
         /// @example
-        /// ```
         /// if (toggler->isToggled()) {
         ///     log::info("Feature is enabled");
         /// }
-        /// ```
         bool isToggled() const;
 
         /// Set whether the toggle is on.
@@ -134,12 +122,10 @@ namespace geode {
         /// @param triggerCallback Whether to call the toggle callback.
         ///
         /// @example
-        /// ```
         /// toggler->setToggled(true);
         ///
         /// // Change the state without calling the callback.
         /// toggler->setToggled(false, false);
-        /// ```
         void setToggled(bool toggled, bool triggerCallback = false);
 
         /// Toggle between the on and off states.
@@ -147,21 +133,17 @@ namespace geode {
         /// @param triggerCallback Whether to call the toggle callback.
         ///
         /// @example
-        /// ```
         /// toggler->toggle();
         ///
         /// // Toggle without calling the callback.
         /// toggler->toggle(false);
-        /// ```
         void toggle(bool triggerCallback = true);
 
         /// Get the node shown when the toggle is off.
         ///
         /// @example
-        /// ```
         /// auto offNode = toggler->getOffNode();
         /// offNode->setOpacity(200);
-        /// ```
         cocos2d::CCNode* getOffNode() const;
 
         /// Change the node shown when the toggle is off.
@@ -169,19 +151,15 @@ namespace geode {
         /// @param node New off node.
         ///
         /// @example
-        /// ```
         /// auto offNode = CCSprite::createWithSpriteFrameName("GJ_checkOff_001.png");
         /// toggler->setOffNode(offNode);
-        /// ```
         void setOffNode(cocos2d::CCNode* node);
 
         /// Get the node shown when the toggle is on.
         ///
         /// @example
-        /// ```
         /// auto onNode = toggler->getOnNode();
         /// onNode->setScale(0.8f);
-        /// ```
         cocos2d::CCNode* getOnNode() const;
 
         /// Change the node shown when the toggle is on.
@@ -189,10 +167,8 @@ namespace geode {
         /// @param node New on node.
         ///
         /// @example
-        /// ```
         /// auto onNode = CCSprite::createWithSpriteFrameName("GJ_checkOn_001.png");
         /// toggler->setOnNode(onNode);
-        /// ```
         void setOnNode(cocos2d::CCNode* node);
 
         /// Change the callback.
@@ -200,7 +176,6 @@ namespace geode {
         /// @param callback New callback.
         ///
         /// @example
-        /// ```
         /// toggler->setToggleCallback([](Toggler* sender, bool isToggled) {
         ///     if (isToggled) {
         ///         log::info("Enabled");
@@ -208,7 +183,6 @@ namespace geode {
         ///         log::info("Disabled");
         ///     }
         /// });
-        /// ```
         void setToggleCallback(TogglerCallback callback);
 
         virtual void activate() override;
